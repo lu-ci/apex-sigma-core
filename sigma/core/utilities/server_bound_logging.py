ac7704468998c3f9ee323a2602870de560dc233d
AXAz0r <thealeksaradovic@gmail.com>
2017-07-15 23:42:10 +0200

    Logging and Plugin Update

---
+++ sigma/core/utilities/server_bound_logging.py
@@ -0,0 +1,12 @@
+import discord
+
+
+async def log_event(db, guild, embed):
+    log_channel_id = db.get_guild_settings(guild.id, 'LoggingChannel')
+    if log_channel_id:
+        log_channel = discord.utils.find(lambda x: x.id == log_channel_id, guild.channels)
+        if log_channel:
+            try:
+                await log_channel.send(embed=embed)
+            except discord.ClientException:
+                pass
