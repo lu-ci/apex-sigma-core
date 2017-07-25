24d33babd537283f4565fc6d3ffa042b8202b1b4
AXAz0r <thealeksaradovic@gmail.com>
2017-07-25 18:13:18 +0200

    Add a Py Version Assertion Check

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3.6
-
+import sys
+import errno
 from sigma.core.sigma import ApexSigma
+
+try:
+    assert sys.version_info >= (3, 6)
+except AssertionError:
+    print('Fatal Error: Wrong Python Version! Sigma supports Python 3.6+!')
+    exit(errno.EINVAL)
 
 if __name__ == '__main__':
     sigma = ApexSigma()
