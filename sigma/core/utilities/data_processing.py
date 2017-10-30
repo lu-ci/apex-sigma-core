c960e9780be8992ea238de03fffe4d4658c17053
AXAz0r <thealeksaradovic@gmail.com>
2017-10-30 12:11:41 +0100

    Remove Redundant Escape

--- sigma/core/utilities/data_processing.py
+++ sigma/core/utilities/data_processing.py
@@ -91,7 +91,7 @@ async def search_youtube(qry):
     async with aiohttp.ClientSession() as session:
         async with session.get(f'{url_base}', params=params) as data:
             html_content = await data.text()
-            search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content)
+            search_results = re.findall(r'href=\"/watch\?v=(.{11})', html_content)
             video_url = f'https://www.youtube.com/watch?v={search_results[0]}'
     return video_url
 
