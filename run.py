7a07443f242ddac7397fa51e3aab517b2eb7c16c
AXAz0r <thealeksaradovic@gmail.com>
2017-11-09 15:35:07 +0100

    Automatic Import Reordering

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 import errno
-import sys
 import os
+import sys
 
 from sigma.core.sigma import ApexSigma
 
--- sigma/modules/fun/auto_response/nablie_plant.py
+++ sigma/modules/fun/auto_response/nablie_plant.py
@@ -1,4 +1,5 @@
 import secrets
+
 import discord
 
 responses = {
