4760d705057c3e2a9a8c8967cf381f0b41c90ad1
AXAz0r <thealeksaradovic@gmail.com>
2017-11-10 11:33:00 +0100

    Proper Core Docstrings

--- sigma/core/sigma.py
+++ sigma/core/sigma.py
@@ -49,6 +49,14 @@ class ApexSigma(client_class):
             Apex Sigma's central core.
             Loads and initializes all core submodules.
             Handles all Discord events and command calls.
+            Class container of Apex Sigma initialized with
+            a discord.py client class.
+            If the client is set to be an official bot client
+            the class is set to discord.AutoShardedClient(),
+            otherwise, if the bot is set to run on a user account,
+            as a self-bot, it uses the discord.Client() class.
+            Resposible for handling events with re-based asyncronous
+            `on_<event>` event calls.
         """
         super().__init__()
         self.ready = False
@@ -82,18 +90,35 @@ class ApexSigma(client_class):
 
     @staticmethod
     def create_cache():
-        """Cleans and creates the cache folder."""
+        """
+        A simple method launched on startup.
+        Cleans and creates the cache folder.
+        If the folder doesn't exist, it will make it,
+        if it does, it will first delete it.
+        :return:
+        """
         if os.path.exists('cache'):
             shutil.rmtree('cache')
         os.makedirs('cache')
 
     def init_logger(self):
-        """Initializes the Logger."""
+        """
+        Launches on startup.
+        Creates a logger for the main core.
+        The logger is appended to the core as a .log attribute.
+        :return:
+        """
         self.log = create_logger('Sigma')
         self.log.info('Logger Created')
 
     def init_config(self):
-        """Loads and initializes the configuration."""
+        """
+        When the client class is initialized and loaded up
+        it will load all of the data from the required files
+        located in the /config/core folder.
+        The configuration is appended as a .cfg attribute.
+        :return:
+        """
         self.log.info('Loading Configuration...')
         self.cfg = init_cfg
         self.log.info(f'Running as a Bot: {self.cfg.dsc.bot}')
@@ -101,7 +126,15 @@ class ApexSigma(client_class):
         self.log.info('Core Configuration Data Loaded')
 
     def init_database(self):
-        """Establishes a database connection."""
+        """
+        Initializes a connection to the MongoDB server.
+        It will try to test the database connection afterwards.
+        It will fail in case the server cannot be connected to,
+        or if the credentials for the MongoDB login are invalid,
+        responding with the appropriate error code.
+        The database is appended as a .db attribute to the core.
+        :return:
+        """
         self.log.info('Connecting to Database...')
         self.db = Database(self, self.cfg.db)
         try:
@@ -115,25 +148,49 @@ class ApexSigma(client_class):
         self.log.info('Successfully Connected to Database')
 
     def init_cool_down(self):
-        """Adds the Cooldown Control class."""
+        """
+        Loads and appends the cooldown control class to the core.
+        The class is appended as a .cd attribute to the core.
+        :return:
+        """
         self.log.info('Loading Cool-down Controls...')
         self.cool_down = CooldownControl(self)
         self.log.info('Cool-down Controls Successfully Enabled')
 
     def init_music(self):
-        """Loads and adds the Music core controls."""
+        """
+        Creates and initializes the music control core.
+        The class is appended as a .music attribute to the core.
+        :return:
+        """
         self.log.info('Loading Music Controller...')
         self.music = MusicCore(self)
         self.log.info('Music Controller Initialized and Ready')
 
     def init_modules(self, init=False):
-        """Loads all modules, events, commands."""
+        """
+        The method for calling and creating the plugin manager.
+        Which in turn loads all event and command modules.
+        Appended to the core as a .modules attribute.
+        :param init:
+        :return:
+        """
         if init:
             self.log.info('Loading Sigma Modules')
         self.modules = PluginManager(self, init)
 
     def get_prefix(self, message):
-        """Retrieves the prefix based on settings."""
+        """
+        Retrieves the prefix based on the message argument.
+        If the message is not in a DM,
+        and if the guild the message came from has a custom prefix,
+        it will return that custom prefix.
+        Otherwise, if the message was used in a DM,
+        or the server doesn't have a custom prefix,
+        the default prefix from the preferences is returned.
+        :param message:
+        :return:
+        """
         prefix = self.cfg.pref.prefix
         if message.guild:
             pfx_search = self.db.get_guild_settings(message.guild.id, 'Prefix')
@@ -142,7 +199,11 @@ class ApexSigma(client_class):
         return prefix
 
     def run(self):
-        """Starts the Discord connection process."""
+        """
+        Starts up the Discord gateway connection process.
+        It will fail in case the token is invalid.
+        :return:
+        """
         try:
             self.log.info('Connecting to Discord Gateway...')
             super().run(self.cfg.dsc.token, bot=self.cfg.dsc.bot)
@@ -151,7 +212,14 @@ class ApexSigma(client_class):
             exit(errno.EPERM)
 
     async def event_runner(self, event_name, *args):
-        """Generic handler for all event calls."""
+        """
+        Genertic method for handling all event calls.
+        When received, regardless of type, they are queued.
+        And will await execution like everything else.
+        :param event_name:
+        :param args:
+        :return:
+        """
         if event_name in self.modules.events:
             for event in self.modules.events[event_name]:
                 # self.loop.create_task(event.execute(*args))
@@ -159,20 +227,31 @@ class ApexSigma(client_class):
                 await self.queue.queue.put(task)
 
     async def on_connect(self):
-        """Emits an event when the client connets."""
+        """
+        Emits an event when the client establishes a connection.
+        :return:
+        """
         event_name = 'connect'
         if event_name in self.modules.events:
             for event in self.modules.events[event_name]:
                 self.loop.create_task(event.execute())
 
     async def on_shard_ready(self, shard_id):
-        """Emits an event when a shard is ready."""
+        """
+        Emits an event when a shard is marked as ready.
+        :param shard_id:
+        :return:
+        """
         self.log.info(f'Connection to Discord Shard #{shard_id} Established')
         event_name = 'shard_ready'
         self.loop.create_task(self.event_runner(event_name, shard_id))
 
     async def on_ready(self):
-        """Emits an event when the entire client is ready."""
+        """
+        Emits an event when the entire client is marked as ready.
+        As well as print out some basic information.
+        :return:
+        """
         self.ready = True
         self.log.info('---------------------------------')
         self.log.info('Apex Sigma Fully Loaded and Ready')
@@ -187,12 +266,17 @@ class ApexSigma(client_class):
             self.launched = True
         self.log.info('All On-Ready Module Loops Created')
         self.log.info('---------------------------------')
-        if os.getenv('CI_TOKEN'):
-            self.log.info('Continuous Integration Environment Detected')
-            exit()
 
     def get_cmd_and_args(self, message, args, mention=False):
-        """Gets the command and arguemnts from the message."""
+        """
+        Parses the message contents for the command and arguments.
+        Changes the pattern for detection if the command is used
+        with a mention start instead of a prefix.
+        :param message:
+        :param args:
+        :param mention:
+        :return:
+        """
         args = list(filter(lambda a: a != '', args))
         if mention:
             if args:
@@ -204,7 +288,14 @@ class ApexSigma(client_class):
         return cmd, args
 
     def clean_self_mentions(self, message):
-        """Removes self mentions in commands that don't use a prefix."""
+        """
+        In case a command is started with a mention
+        and not a command prefix, this function removes
+        mentions of the bot itself from the message mentions.
+        This is to prevent miss-targetting with commands.
+        :param message:
+        :return:
+        """
         for mention in message.mentions:
             if mention.id == self.user.id:
                 message.mentions.remove(mention)
@@ -212,10 +303,13 @@ class ApexSigma(client_class):
 
     async def on_message(self, message):
         """
-        Emits a message event when any message is sent.
-        Sends the message content through get_cmd_and_args()
-        and returns the results if the message contains a command.
-        If it does, the appropriate command will be called.
+        Emits an event when a message is read.
+        Depending on the message contents, it will also
+        launch commands if the command parser detects one.
+        If the command has a mention of the bot, it will
+        emit an event for that as well.
+        :param message:
+        :return:
         """
         self.message_count += 1
         if not message.author.bot:
@@ -248,46 +342,85 @@ class ApexSigma(client_class):
                     await self.queue.queue.put(task)
 
     async def on_message_edit(self, before, after):
-        """Emits an event when a message is edited."""
+        """
+        Emits an event when a message is edited.
+        :param before:
+        :param after:
+        :return:
+        """
         if not before.author.bot:
             event_name = 'message_edit'
             self.loop.create_task(self.event_runner(event_name, before, after))
 
     async def on_message_delete(self, message):
-        """Emits an event when a message is deleted."""
+        """
+        Emits an event when a message is deleted.
+        :param message:
+        :return:
+        """
         if not message.author.bot:
             event_name = 'message_delete'
             self.loop.create_task(self.event_runner(event_name, message))
 
     async def on_member_join(self, member):
-        """Emits an event when a member joins a guild."""
+        """
+        Emits an event when a member joins a guild.
+        :param member:
+        :return:
+        """
         if not member.bot:
             event_name = 'member_join'
             self.loop.create_task(self.event_runner(event_name, member))
 
     async def on_member_remove(self, member):
-        """Emits an event when a member leaves a guild."""
+        """
+        Emits an event when a member leaves a guild.
+        :param member:
+        :return:
+        """
         if not member.bot:
             event_name = 'member_remove'
             self.loop.create_task(self.event_runner(event_name, member))
 
     async def on_member_update(self, before, after):
-        """Emits an event when a member updates their profile."""
+        """
+        Emits an event when a member changes their profiles.
+        This can be a name change, nickname change, status change,
+        game change, avatar change.
+        :param before:
+        :param after:
+        :return:
+        """
         if not before.bot:
             event_name = 'member_update'
             self.loop.create_task(self.event_runner(event_name, before, after))
 
     async def on_guild_join(self, guild):
-        """Emits an event when the client joins a guild."""
+        """
+        Emits an event when the client joins a guild.
+        :param guild:
+        :return:
+        """
         event_name = 'guild_join'
         self.loop.create_task(self.event_runner(event_name, guild))
 
     async def on_guild_remove(self, guild):
-        """Emits an event when the client leaves a guild."""
+        """
+        Emit an event when the client leaves a guild.
+        :param guild:
+        :return:
+        """
         event_name = 'guild_remove'
         self.loop.create_task(self.event_runner(event_name, guild))
 
     async def on_voice_state_update(self, member, before, after):
-        """Emits an event when a member changes their voice state."""
+        """
+        Emits an event when a member changes their voice state.
+        Connets, disconnects, moves or is (un)muted or (un)deafened.
+        :param member:
+        :param before:
+        :param after:
+        :return:
+        """
         event_name = 'voice_state_update'
         self.loop.create_task(self.event_runner(event_name, member, before, after))
