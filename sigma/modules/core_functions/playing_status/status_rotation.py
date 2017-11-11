35385ae1ac898105deaf028231c7051852d7c58b
AXAz0r <thealeksaradovic@gmail.com>
2017-11-11 19:12:37 +0100

    Status Hashing and Cutting

--- sigma/modules/core_functions/playing_status/status_rotation.py
+++ sigma/modules/core_functions/playing_status/status_rotation.py
@@ -1,9 +1,22 @@
 import asyncio
+import hashlib
 import secrets
 
 import discord
 
 status_cache = []
+
+
+def rancom_capitalize(text):
+    new_text = ''
+    char_list = list(text)
+    while char_list:
+        char_choice = char_list.pop(0)
+        cap_roll = secrets.randbelow(2)
+        if cap_roll == 0:
+            char_choice = char_choice.upper()
+        new_text += char_choice
+    return new_text
 
 
 async def status_rotation(ev):
@@ -21,9 +34,21 @@ async def status_clockwork(ev):
                     status_cache.append(status_text)
             if status_cache:
                 status = status_cache.pop(secrets.randbelow(len(status_cache)))
+                mode_roll = secrets.randbelow(3)
+                if mode_roll == 0:
+                    hashes = hashlib.algorithms_guaranteed
+                    hgen = hashlib.new(secrets.choice(hashes))
+                    hgen.update(status.encode('utf-8'))
+                    digest = hgen.hexdigest()
+                    cut = secrets.randbelow(11)
+                    cut_text = digest[cut:-(cut + 10)]
+                    status = rancom_capitalize(cut_text)
+                    sign_roll = secrets.randbelow(2)
+                    if sign_roll:
+                        status += '='
                 game = discord.Game(name=status)
                 try:
                     await ev.bot.change_presence(game=game)
                 except discord.ConnectionClosed:
                     pass
-        await asyncio.sleep(180)
+        await asyncio.sleep(60)
