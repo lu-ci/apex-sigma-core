b50652bf3b1457e02052b7789cc72a0e33b6e4bc
AXAz0r <thealeksaradovic@gmail.com>
2017-11-15 02:30:02 +0100

    Revert "Queue Execution Measuring"
    This reverts commit ffa9d86c94fafa0c7350555a7d217bc17e4c09f8.

--- sigma/core/mechanics/threading.py
+++ sigma/core/mechanics/threading.py
@@ -1,29 +1,18 @@
-import arrow
 import asyncio
-
-from sigma.core.mechanics.logger import create_logger
+import functools
+from concurrent.futures import ThreadPoolExecutor
 
 
 class QueueControl(object):
     def __init__(self):
-        self.log = create_logger('Queue Core')
         self.loop = asyncio.get_event_loop()
-        self.queue = asyncio.Queue(maxsize=500)
+        self.queue = asyncio.Queue()
         self.loop.create_task(self.queue_loop())
+        self.threads = ThreadPoolExecutor(2)
 
     async def queue_loop(self):
         while True:
-            start_stamp = arrow.utcnow().float_timestamp
             item, *args = await self.queue.get()
-            try:
-                await item.execute(*args)
-            except Exception:
-                self.log.error(f'Failed to execute a queue item: {item.name}')
-            end_stamp = arrow.utcnow().float_timestamp
-            diff = round(end_stamp - start_stamp, 5)
-            exec_time = f'{item.name} Execution Time: {diff}'
-            if diff <= 5:
-                self.log.debug(exec_time)
-            else:
-                self.log.warning(exec_time)
-
+            # item.execute, *args
+            task = functools.partial(self.loop.create_task, item.execute(*args))
+            await self.loop.run_in_executor(self.threads, task)
