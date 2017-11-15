6216b4b093925a711a55a66b9329c00c9108197d
AXAz0r <thealeksaradovic@gmail.com>
2017-11-15 02:42:19 +0100

    Revert "Threading Loops"
    This reverts commit 4beb75fc14c8f2db124603911e17ac8791f874f9.

--- sigma/core/mechanics/threading.py
+++ sigma/core/mechanics/threading.py
@@ -1,4 +1,6 @@
 import asyncio
+import functools
+from concurrent.futures import ThreadPoolExecutor
 
 
 class QueueControl(object):
@@ -6,8 +8,11 @@ class QueueControl(object):
         self.loop = asyncio.get_event_loop()
         self.queue = asyncio.Queue()
         self.loop.create_task(self.queue_loop())
+        self.threads = ThreadPoolExecutor(2)
 
     async def queue_loop(self):
         while True:
             item, *args = await self.queue.get()
-            self.loop.create_task(item.execute(*args))
+            # item.execute, *args
+            task = functools.partial(self.loop.create_task, item.execute(*args))
+            await self.loop.run_in_executor(self.threads, task)
