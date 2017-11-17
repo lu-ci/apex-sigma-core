1d33ce22e5ecc14539fe06229a70c5cdd943d462
AXAz0r <thealeksaradovic@gmail.com>
2017-11-17 21:55:29 +0100

    Remove Joke Events

--- sigma/modules/fun/auto_response/press_f_adder.py
+++
@@ -1,7 +0,0 @@
-async def press_f_adder(ev, message):
-    if message.content.lower() == 'f':
-        if message.guild:
-            respond = ev.db.get_guild_settings(message.guild.id, 'AutoResponder')
-            if respond:
-                if message.guild.me.permissions_in(message.channel).add_reactions:
-                    await message.add_reaction('🇫')
