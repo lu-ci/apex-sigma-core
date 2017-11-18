ecb95ed81a7769f41200b4ed8ddd0debf5778272
AXAz0r <thealeksaradovic@gmail.com>
2017-11-18 14:45:46 +0100

    Time Conversion and Current Commands

---
+++ sigma/modules/utilities/mathematics/currenttime.py
@@ -0,0 +1,39 @@
+import arrow
+import discord
+import yaml
+from arrow.parser import ParserError
+
+tz_aliases = None
+tz_offsets = None
+
+
+async def currenttime(cmd, message, args):
+    global tz_aliases
+    global tz_offsets
+    if not tz_aliases:
+        with open(cmd.resource('tz_aliases.yml')) as tz_a_file:
+            tz_aliases = yaml.safe_load(tz_a_file)
+    if not tz_offsets:
+        with open(cmd.resource('tz_offsets.yml')) as tz_o_file:
+            tz_offsets = yaml.safe_load(tz_o_file)
+    if args:
+        shift = ' '.join(args).lower()
+        if shift in tz_aliases:
+            shift = tz_aliases.get(shift)
+        if shift in tz_offsets:
+            shift = tz_offsets.get(shift)
+    else:
+        shift = None
+    try:
+        if shift:
+            now = arrow.utcnow().to(shift)
+        else:
+            now = arrow.utcnow()
+    except ParserError:
+        now = None
+    if now:
+        time_out = now.format('DD. MMM. YYYY - HH:mm:ss')
+        response = discord.Embed(color=0xf9f9f9, title=f'🕥 {time_out}')
+    else:
+        response = discord.Embed(color=0xBE1931, title='❗ Could not parse that time.')
+    await message.channel.send(embed=response)
--- sigma/modules/utilities/mathematics/timeconvert.py
+++ sigma/modules/utilities/mathematics/timeconvert.py
@@ -1,8 +1,21 @@
 import arrow
 import discord
+import yaml
+from arrow.parser import ParserError
+
+tz_aliases = None
+tz_offsets = None
 
 
 async def timeconvert(cmd, message, args):
+    global tz_aliases
+    global tz_offsets
+    if not tz_aliases:
+        with open(cmd.resource('tz_aliases.yml')) as tz_a_file:
+            tz_aliases = yaml.safe_load(tz_a_file)
+    if not tz_offsets:
+        with open(cmd.resource('tz_offsets.yml')) as tz_o_file:
+            tz_offsets = yaml.safe_load(tz_o_file)
     if args:
         conv_input = ' '.join(args).split('>')
         if len(conv_input) == 2:
@@ -10,12 +23,26 @@ async def timeconvert(cmd, message, args):
             if len(from_pieces) == 2:
                 from_time = from_pieces[0]
                 from_zone = from_pieces[1]
+                if from_zone.lower() in tz_aliases:
+                    from_zone = tz_aliases.get(from_zone.lower())
+                if from_zone.lower() in tz_offsets:
+                    from_zone = tz_offsets.get(from_zone.lower())
                 to_zone = conv_input[1]
-                from_string = f'{arrow.utcnow().format("YYYY-MM-DD")} {from_time}:00'
-                from_arrow = arrow.get(arrow.get(from_string).datetime, from_zone)
-                to_arrow = from_arrow.to(to_zone)
-                time_out = to_arrow.format('YYYY-MM-DD HH:mm:ss (ZZ GMT)')
-                response = discord.Embed(color=0x696969, title=f'🕥 {time_out}')
+                if to_zone.lower() in tz_aliases:
+                    to_zone = tz_aliases.get(to_zone.lower())
+                if to_zone.lower() in tz_offsets:
+                    to_zone = tz_offsets.get(to_zone.lower())
+                try:
+                    from_string = f'{arrow.utcnow().format("YYYY-MM-DD")} {from_time}:00'
+                    if from_zone != 0:
+                        from_arrow = arrow.get(from_string).to(str(from_zone))
+                    else:
+                        from_arrow = arrow.get(from_string)
+                    to_arrow = from_arrow.to(str(to_zone))
+                    time_out = to_arrow.format('DD. MMM. YYYY - HH:mm:ss (ZZ)')
+                    response = discord.Embed(color=0xf9f9f9, title=f'🕥 {time_out}')
+                except ParserError:
+                    response = discord.Embed(color=0xBE1931, title='❗ Could not parse that time.')
             else:
                 response = discord.Embed(color=0xBE1931, title='❗ Invalid first argument.')
         else:
