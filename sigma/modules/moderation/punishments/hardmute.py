c412d6222071a069cffc9c91656ff743d44abd39
AXAz0r <thealeksaradovic@gmail.com>
2017-11-22 03:13:26 +0100

    Hardmute Notifies Target

--- sigma/modules/moderation/punishments/hardmute.py
+++ sigma/modules/moderation/punishments/hardmute.py
@@ -42,6 +42,14 @@ async def hardmute(cmd, message, args):
                     await log_event(cmd.db, message.guild, log_embed)
                     title = f'✅ {target.name}#{target.discriminator} has been hard-muted.'
                     response = discord.Embed(color=0x77B255, title=title)
+                    to_target_title = f'🔇 You have been hard-muted.'
+                    to_target = discord.Embed(color=0x696969)
+                    to_target.add_field(name=to_target_title, value=f'Reason: {reason}')
+                    to_target.set_footer(text=f'On: {message.guild.name}', icon_url=message.guild.icon_url)
+                    try:
+                        await target.send(embed=to_target)
+                    except discord.Forbidden:
+                        pass
                 else:
                     response = discord.Embed(color=0xBE1931, title='❗ That user is euqal or above you.')
             else:
