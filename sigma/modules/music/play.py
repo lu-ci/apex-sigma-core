29522e4f7f0e3c61c0efbc8f74db807828271c01
AXAz0r <thealeksaradovic@gmail.com>
2017-11-23 18:44:34 +0100

    Revert "Add More Voice Client Checks"
    This reverts commit ffd258e1928310b717d17cabbd6da84fa3260950.

--- sigma/modules/music/play.py
+++ sigma/modules/music/play.py
@@ -9,12 +9,11 @@ from sigma.core.utilities.stats_processing import add_special_stats
 
 def player_listening(voice_client):
     user_count = 0
-    if voice_client:
-        for member in voice_client.channel.members:
-            if not member.bot:
-                if not member.voice.self_deaf:
-                    if not member.voice.deaf:
-                        user_count += 1
+    for member in voice_client.channel.members:
+        if not member.bot:
+            if not member.voice.self_deaf:
+                if not member.voice.deaf:
+                    user_count += 1
     if user_count:
         active = True
     else:
@@ -76,8 +75,7 @@ async def play(cmd, message, args):
                         else:
                             active = False
                             break
-                    if message.guild.voice_client:
-                        message.guild.voice_client.stop()
+                    message.guild.voice_client.stop()
                     if not active:
                         break
                 response = discord.Embed(color=0x3B88C3, title='🎵 Queue complete.')
