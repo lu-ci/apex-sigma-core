a60268d292a9a012a2b8777f7dfa4c07a95f2174
AXAz0r <thealeksaradovic@gmail.com>
2017-11-28 17:29:40 +0100

    Self Updating Reaction IDs and ID in Footer

--- sigma/modules/interactions/mech/interaction_mechanics.py
+++ sigma/modules/interactions/mech/interaction_mechanics.py
@@ -1,8 +1,22 @@
+import copy
 import secrets
 
 import discord
 
 interaction_cache = {}
+
+
+def update_id(db, interaction):
+    new_id = secrets.token_hex(4)
+    new_data = copy.deepcopy(interaction)
+    new_data.update({'ReactionID': new_id})
+    db[db.db_cfg.database]['Interactions'].update_one(interaction, {'$set': new_data})
+
+
+def get_interaction_list(db, intername):
+    interactions = db[db.db_cfg.database]['Interactions'].find({'Name': intername})
+    interactions = list(interactions)
+    return interactions
 
 
 def grab_interaction(db, intername):
@@ -14,13 +28,20 @@ def grab_interaction(db, intername):
         else:
             fill = False
     if fill:
-        interactions = db[db.db_cfg.database]['Interactions'].find({'Name': intername})
-        interactions = list(interactions)
+        interactions = get_interaction_list(db, intername)
+        refill = False
+        for interaction in interactions:
+            inter_id = interaction.get('ReactionID')
+            if not inter_id:
+                update_id(db, interaction)
+                refill = True
+        if refill:
+            interactions = get_interaction_list(db, intername)
         interaction_cache.update({intername: interactions})
     if interaction_cache[intername]:
         choice = interaction_cache[intername].pop(secrets.randbelow(len(interaction_cache[intername])))
     else:
-        choice = {'URL': 'https://i.imgur.com/m59E4nx.gif', 'UserID': None, 'ServerID': None}
+        choice = {'URL': 'https://i.imgur.com/m59E4nx.gif', 'UserID': None, 'ServerID': None, 'ReactionID': None}
     return choice
 
 
@@ -39,20 +60,18 @@ def get_target(message):
 
 
 def make_footer(cmd, item):
-    if item['UserID']:
-        uid = item['UserID']
-        user = discord.utils.find(lambda x: x.id == uid, cmd.bot.get_all_members())
-        if user:
-            username = user.name
-        else:
-            username = 'Unknown User'
+    uid = item.get('UserID')
+    user = discord.utils.find(lambda x: x.id == uid, cmd.bot.get_all_members())
+    if user:
+        username = user.name
     else:
         username = 'Unknown User'
-    sid = item['ServerID']
+    sid = item.get('ServerID')
     srv = discord.utils.find(lambda x: x.id == sid, cmd.bot.guilds)
     if srv:
         servername = srv.name
     else:
         servername = 'Unknown Server'
-    footer = f'Submitted by {username} from {servername}.'
+    react_id = item.get('ReactionID')
+    footer = f'[{react_id}] | Submitted by {username} from {servername}.'
     return footer
