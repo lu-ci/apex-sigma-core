99b55e202fd0704f9a9301bb6c874fac5f6d4828
AXAz0r <thealeksaradovic@gmail.com>
2017-12-04 12:19:02 +0100

    Kud Transfering Command

---
+++ sigma/modules/fun/economy/givecurrency.py
@@ -0,0 +1,31 @@
+import discord
+
+
+async def givecurrency(cmd, message, args):
+    if args:
+        if len(args) >= 2:
+            if message.mentions:
+                target = message.mentions[0]
+                try:
+                    amount = int(args[0])
+                except ValueError:
+                    amount = None
+                if amount:
+                    current_kud = cmd.db.get_currency(message.author, message.guild)['current']
+                    if current_kud >= amount:
+                        amount_end = int(amount * 0.95)
+                        cmd.db.rmv_currency(message.author, amount)
+                        cmd.db.add_currency(target, message.guild, amount_end, additive=False)
+                        title = f'✅ Transfered {amount_end} to {target.display_name}.'
+                        response = discord.Embed(color=0x77B255, title=title)
+                    else:
+                        response = discord.Embed(color=0xa7d28b, title=f'💸 You don\'t have that much.')
+                else:
+                    response = discord.Embed(color=0xBE1931, title='❗ Invalid amount.')
+            else:
+                response = discord.Embed(color=0xBE1931, title='❗ No user was mentioned.')
+        else:
+            response = discord.Embed(color=0xBE1931, title='❗ No user was mentioned.')
+    else:
+        response = discord.Embed(color=0xBE1931, title='❗ No user was mentioned.')
+    await message.channel.send(embed=response)
