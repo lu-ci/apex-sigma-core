e66279ed197833f1c3c6552a5a4b7b6e2e29d380
AXAz0r <thealeksaradovic@gmail.com>
2017-12-08 01:51:13 +0100

    Remove Responder Fix

--- sigma/modules/moderation/server_settings/custom_commands/removeresponder.py
+++ sigma/modules/moderation/server_settings/custom_commands/removeresponder.py
@@ -6,12 +6,15 @@ async def removeresponder(cmd, message, args):
         if args:
             trigger = args[0].lower()
             if trigger not in cmd.bot.modules.commands and trigger not in cmd.bot.modules.alts:
-                auto_respones = await cmd.db.get_guild_settings(message.guild.id, 'ResponderTriggers')
-                if auto_respones is None:
+                auto_responses = await cmd.db.get_guild_settings(message.guild.id, 'ResponderTriggers')
+                if auto_responses is None:
                     auto_respones = {}
-                del auto_respones[trigger]
-                await cmd.db.set_guild_settings(message.guild.id, 'ResponderTriggers', auto_respones)
-                response = discord.Embed(title=f'✅ {trigger} has been removed.', color=0x66CC66)
+                if trigger in auto_responses:
+                    del auto_responses[trigger]
+                    await cmd.db.set_guild_settings(message.guild.id, 'ResponderTriggers', auto_responses)
+                    response = discord.Embed(title=f'✅ {trigger} has been removed.', color=0x66CC66)
+                else:
+                    response = discord.Embed(title='❗ I didn\'t find such a trigger.', color=0xBE1931)
             else:
                 response = discord.Embed(title='❗ Can\'t have the same name as a core command.', color=0xBE1931)
         else:
