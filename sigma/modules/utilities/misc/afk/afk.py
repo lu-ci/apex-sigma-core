64df615572cbca108cc075f4179396e12459d58a
AXAz0r <thealeksaradovic@gmail.com>
2018-01-06 12:57:54 +0100

    AFK Status Update If Exists

--- sigma/modules/utilities/misc/afk/afk.py
+++ sigma/modules/utilities/misc/afk/afk.py
@@ -4,29 +4,33 @@ import discord
 
 async def afk(cmd, message, args):
     afk_data = await cmd.db[cmd.db.db_cfg.database]['AwayUsers'].find_one({'UserID': message.author.id})
-    if afk_data:
-        response = discord.Embed(color=0xBE1931, title='❗ You are already marked as AFK.')
+    if args:
+        afk_reason = ' '.join(args)
     else:
-        if args:
-            afk_reason = ' '.join(args)
-        else:
-            afk_reason = 'No reason stated.'
-        in_data = {
-            'UserID': message.author.id,
-            'Timestamp': arrow.utcnow().timestamp,
-            'Reason': afk_reason
-        }
+        afk_reason = 'No reason stated.'
+    in_data = {
+        'UserID': message.author.id,
+        'Timestamp': arrow.utcnow().timestamp,
+        'Reason': afk_reason
+    }
+    if afk_data:
+        title = 'Your status has been updated.'
+        await cmd.db[cmd.db.db_cfg.database]['AwayUsers'].update_one({'UserID': message.author.id}, {'$set': in_data})
+    else:
+        title = 'You have been marked as away.'
         await cmd.db[cmd.db.db_cfg.database]['AwayUsers'].insert_one(in_data)
-        url = None
-        for piece in afk_reason.split():
-            if piece.startswith('http'):
-                suffix = piece.split('.')[-1]
-                if suffix in ['gif', 'jpg', 'jpeg', 'png']:
-                    url = piece
-                    afk_reason = afk_reason.replace(piece, '')
-                    break
-        response = discord.Embed(color=0x66CC66)
-        response.add_field(name='✅ You have been marked as afk.', value=f'Reason: **{afk_reason}**')
-        if url:
-            response.set_image(url=url)
+    url = None
+    for piece in afk_reason.split():
+        if piece.startswith('http'):
+            suffix = piece.split('.')[-1]
+            if suffix in ['gif', 'jpg', 'jpeg', 'png']:
+                url = piece
+                afk_reason = afk_reason.replace(piece, '')
+                break
+    if not afk_reason:
+        afk_reason = 'See image below.'
+    response = discord.Embed(color=0x66CC66)
+    response.add_field(name=f'✅ {title}.', value=f'Reason: **{afk_reason}**')
+    if url:
+        response.set_image(url=url)
     await message.channel.send(embed=response)
