4a7d55aed20dac1a77a6223dd1f7730ed8425417
AXAz0r <thealeksaradovic@gmail.com>
2018-01-21 18:27:23 +0100

    Fix LogKicks Function Name

--- sigma/modules/moderation/server_settings/logging/logkicks.py
+++ sigma/modules/moderation/server_settings/logging/logkicks.py
@@ -4,7 +4,7 @@ from sigma.core.mechanics.command import SigmaCommand
 from sigma.core.utilities.generic_responses import permission_denied
 
 
-async def logbans(cmd: SigmaCommand, message: discord.Message, args: list):
+async def logkicks(cmd: SigmaCommand, message: discord.Message, args: list):
     if message.author.guild_permissions.manage_guild:
         log_event = await cmd.db.get_guild_settings(message.guild.id, 'LogKicks')
         if log_event:
