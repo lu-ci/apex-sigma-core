91d27d122f188ecddeeea6fedd6c8a2871fd7860
daegontaven <daegontaven@gmail.com>
2018-02-13 17:15:52 -0500

    Remove unused discord import

--- sigma/modules/moderation/server_settings/roles/autorole/rolepersist_update_handler.py
+++ sigma/modules/moderation/server_settings/roles/autorole/rolepersist_update_handler.py
@@ -1,6 +1,3 @@
-import discord
-
-
 async def rolepersist_update_handler(ev, before, after):
     persist_enabled = await ev.db.get_guild_settings(after.guild.id, 'RolePersist')
     if persist_enabled is not True:
