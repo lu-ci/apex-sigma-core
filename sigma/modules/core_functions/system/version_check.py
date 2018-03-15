a8977cb4bd4f61c4afe8249b703e4a10183b634e
AXAz0r <thealeksaradovic@gmail.com>
2018-03-15 12:34:59 +0100

    Version Failure Expectance

--- sigma/modules/core_functions/system/version_check.py
+++ sigma/modules/core_functions/system/version_check.py
@@ -21,19 +21,26 @@ import aiohttp
 
 async def version_check(ev):
     version_url = 'https://api.lucia.moe/rest/sigma/version'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(version_url) as version_data:
-            data = await version_data.read()
-            data = json.loads(data)
-    official_stamp = data['build_date']
-    version = ev.bot.info.get_version()
-    current_stamp = version.timestamp
-    if official_stamp > current_stamp:
-        current = f'{version.major}.{version.minor}.{version.patch} {version.codename}'
-        latest = f'{data["version"]["major"]}.{data["version"]["minor"]}.{data["version"]["patch"]} {data["codename"]}'
-        ev.log.warning('---------------------------------')
-        ev.log.warning('Your Sigma version is outdated.')
-        ev.log.warning(f'CURRENT: {current}')
-        ev.log.warning(f'LATEST:  {latest}')
-        ev.log.warning('Updating is strongly suggested.')
-        ev.log.warning('---------------------------------')
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(version_url) as version_data:
+                data = await version_data.read()
+                data = json.loads(data)
+    except aiohttp.client_exceptions.ClientConnectorError:
+        data = None
+    if data:
+        official_stamp = data['build_date']
+        version = ev.bot.info.get_version()
+        current_stamp = version.timestamp
+        if official_stamp > current_stamp:
+            current = f'{version.major}.{version.minor}.{version.patch} {version.codename}'
+            latest = f'{data["version"]["major"]}.{data["version"]["minor"]}.{data["version"]["patch"]}'
+            latest += f' {data["codename"]}'
+            ev.log.warning('---------------------------------')
+            ev.log.warning('Your Sigma version is outdated.')
+            ev.log.warning(f'CURRENT: {current}')
+            ev.log.warning(f'LATEST:  {latest}')
+            ev.log.warning('Updating is strongly suggested.')
+            ev.log.warning('---------------------------------')
+    else:
+        ev.log.error('Could not retrieve latest version.')
