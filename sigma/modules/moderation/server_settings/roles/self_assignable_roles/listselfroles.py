e1d71d47af702867a8438d2094517bff6645adc9
AXAz0r <thealeksaradovic@gmail.com>
2018-03-15 12:46:39 +0100

    Paginated Role List

--- sigma/modules/moderation/server_settings/roles/self_assignable_roles/listselfroles.py
+++ sigma/modules/moderation/server_settings/roles/self_assignable_roles/listselfroles.py
@@ -28,23 +28,36 @@ async def listselfroles(cmd: SigmaCommand, message: discord.Message, args: list)
         for role in self_roles:
             if role == srv_role.id:
                 role_list.append(srv_role.name)
+    role_list = sorted(role_list)
     if not role_list:
         embed = discord.Embed(type='rich', color=0x3B88C3, title='ℹ No Self Assignable Roles Set')
     else:
+        if args:
+            page = args[0]
+            try:
+                page = int(page)
+            except ValueError:
+                page = 1
+        else:
+            page = 1
+        if page < 1:
+            page = 1
+        start_range = 10 * (page - 1)
+        end_range = 10 * page
+        role_count = len(role_list)
+        role_list = role_list[start_range:end_range]
+        if len(role_list) > 1:
+            ender = 's'
+        else:
+            ender = ''
+        summary = f'Showing **{len(role_list)}** role{ender} from Page **#{page}**.'
+        summary += f'\n{message.guild.name} has **{role_count}** self assignable role{ender}.'
         rl_out = ''
         role_list = sorted(role_list)
         for rl in role_list:
             rl_out += '\n- ' + rl
-        self_count = len(role_list)
-        if self_count == 1:
-            connector = 'is'
-            ender = 'role'
-        else:
-            connector = 'are'
-            ender = 'roles'
-        stat_line = f'There {connector} {self_count} self assignable {ender}.'
         embed = discord.Embed(color=0x1ABC9C)
         embed.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
-        embed.add_field(name=f'Self Assignable Role Stats', value=stat_line, inline=False)
+        embed.add_field(name=f'Self Assignable Role Stats', value=summary, inline=False)
         embed.add_field(name=f'List of Self Assignable Roles', value=f'{rl_out}', inline=False)
     await message.channel.send(None, embed=embed)
