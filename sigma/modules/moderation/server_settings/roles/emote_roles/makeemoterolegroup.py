0735f7e1bed7ca654fc6366594ebaeebf2337d1a
AXAz0r <thealeksaradovic@gmail.com>
2018-03-23 15:38:21 +0100

    Emote Role Group Creation

---
+++ sigma/modules/moderation/server_settings/roles/emote_roles/makeemoterolegroup.py
@@ -0,0 +1,32 @@
+# Apex Sigma: The Database Giant Discord Bot.
+# Copyright (C) 2017  Lucia's Cipher
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import discord
+import secrets
+from sigma.core.mechanics.command import SigmaCommand
+from sigma.core.utilities.generic_responses import permission_denied
+
+
+async def makeemoterolegroup(cmd: SigmaCommand, message: discord.Message, args: list):
+    if message.author.guild_permissions.manage_guild:
+        emote_groups = await cmd.db.get_guild_settings(message.guild.id, 'EmoteRoleGroups') or {}
+        group_id = secrets.token_hex(3)
+        emote_groups.update({group_id: []})
+        await cmd.db.set_guild_settings(message.guild.id, emote_groups)
+        response = discord.Embed(color=0x66CC66, title=f'✅ Emote role group {group_id} has been created.')
+    else:
+        response = permission_denied("Manage Server")
+    await message.channel.send(embed=response)
