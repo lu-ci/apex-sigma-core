d0b453ac79da1d8fd610a7277f482a9f0da2170a
AXAz0r <thealeksaradovic@gmail.com>
2018-04-04 10:39:15 +0200

    Osu Command Fix

--- sigma/modules/games/osu/osu.py
+++ sigma/modules/games/osu/osu.py
@@ -16,29 +16,43 @@
 
 import aiohttp
 import discord
-from lxml import html
+import json
 
 from sigma.core.mechanics.command import SigmaCommand
 
 osu_logo = 'http://w.ppy.sh/c/c9/Logo.png'
 
 
+async def find_user_data(profile_url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(profile_url) as data:
+            page = await data.text()
+    lines = [x.strip() for x in page.split('\n')]
+    lines.reverse()
+    user_data = {}
+    for line in lines:
+        if line.startswith('{"id":'):
+            try:
+                user_data = json.loads(line)
+                break
+            except json.JSONDecodeError:
+                pass
+    return user_data
+
+
 async def osu(cmd: SigmaCommand, message: discord.Message, args: list):
     if args:
         osu_input = '%20'.join(args)
-        try:
-            profile_url = 'https://osu.ppy.sh/u/' + osu_input
-            async with aiohttp.ClientSession() as session:
-                async with session.get(profile_url) as data:
-                    page = await data.text()
-            root = html.fromstring(page)
-            username = root.cssselect('.profile-username')[0].text[:-1]
+        profile_url = f'https://osu.ppy.sh/users/{osu_input}'
+        user_data = await find_user_data(profile_url)
+        username = user_data.get('username')
+        if username:
             user_color = str(message.author.color)[1:]
             sig_url = f'https://lemmmy.pw/osusig/sig.php?colour=hex{user_color}&uname={osu_input}'
             response = discord.Embed(color=message.author.color)
             response.set_image(url=sig_url)
             response.set_author(name=f'{username}\'s osu! Profile', url=profile_url, icon_url=osu_logo)
-        except IndexError:
+        else:
             response = discord.Embed(color=0xBE1931, title='❗ Unable to retrieve profile.')
     else:
         response = discord.Embed(color=0xBE1931, title='❗ Nothing inputted.')
