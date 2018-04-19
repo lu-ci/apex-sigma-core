f89825331a4d50a92396899094f7537ffcd14267
AXAz0r <thealeksaradovic@gmail.com>
2018-04-19 21:35:18 +0200

    Warning List Paginated

--- sigma/modules/moderation/warning/listwarnings.py
+++ sigma/modules/moderation/warning/listwarnings.py
@@ -28,6 +28,18 @@ async def listwarnings(cmd: SigmaCommand, message: discord.Message, args: list):
             target = message.author
     else:
         target = message.author
+    if args:
+        page = args[-1]
+        try:
+            page = int(page)
+        except ValueError:
+            page = 1
+    else:
+        page = 1
+    if page < 1:
+        page = 1
+    start_range = 5 * (page - 1)
+    end_range = 5 * page
     if target:
         lookup = {'guild': message.guild.id, 'target.id': target.id, 'warning.active': True}
         warnings = await cmd.db[cmd.db.db_cfg.database].Warnings.find(lookup).to_list(None)
@@ -43,11 +55,14 @@ async def listwarnings(cmd: SigmaCommand, message: discord.Message, args: list):
                     moderator = warning.get('moderator').get('name')
                 warn_time = arrow.get(warning.get('warning').get('timestamp')).format('DD. MMM. YYYY. HH:mm')
                 warn_list.append(f'`{warn_id}` by **{moderator}** on {warn_time}.')
+            total_warns = len(warn_list)
+            warn_list = warn_list[start_range:end_range]
             warn_list = '\n'.join(warn_list)
             ending = 'warnings' if len(warnings) > 1 else 'warning'
             start = f'{target.name} has' if target.id != message.author.id else 'You have'
             response = discord.Embed(color=0xFFCC4D)
             response.add_field(name=f'⚠ {start} {len(warnings)} active {ending}.', value=warn_list)
+            response.set_footer(text=f'Showing {len(warn_list)} of {target.name}\'s warns out of {total_warns}.')
         else:
             start = f'{target.name} doesn\'t' if target.id != message.author.id else 'You don\'t'
             response = discord.Embed(color=0x55acee, title=f'💠 {start} have any warnings.')
