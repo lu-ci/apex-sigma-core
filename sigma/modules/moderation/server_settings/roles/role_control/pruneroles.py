a2b9a0087394d5fbde849beda03de2508d8720ee
AXAz0r <thealeksaradovic@gmail.com>
2018-04-23 21:01:01 +0200

    Appropriate Role Prune

--- sigma/modules/moderation/server_settings/roles/role_control/pruneroles.py
+++ sigma/modules/moderation/server_settings/roles/role_control/pruneroles.py
@@ -1,14 +1,28 @@
+# Apex Sigma: The Database Giant Discord Bot.
+# Copyright (C) 2018  Lucia's Cipher
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import discord
 
 from sigma.core.mechanics.command import SigmaCommand
 
+
 async def pruneroles(cmd: SigmaCommand, message: discord.Message, args: list):
     if message.author.guild_permissions.manage_roles:
         empty_roles = list(filter(lambda r: len(r.members) == 0, message.guild.roles))
-
-        for role in empty_roles:
-            await role.delete()
-
+        [await role.delete() for role in empty_roles]
         response = discord.Embed(color=0x77B255, title=f'Removed {len(empty_roles)} roles from this server.')
         await message.channel.send(embed=response)
-
