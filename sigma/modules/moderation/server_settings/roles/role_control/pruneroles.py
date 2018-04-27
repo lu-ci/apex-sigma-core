d70fce63726119fccb6b54a2417b3a4583a85241
AXAz0r <thealeksaradovic@gmail.com>
2018-04-27 13:16:22 +0200

    Prune Roles Checkmark

--- sigma/modules/moderation/server_settings/roles/role_control/pruneroles.py
+++ sigma/modules/moderation/server_settings/roles/role_control/pruneroles.py
@@ -24,5 +24,5 @@ async def pruneroles(cmd: SigmaCommand, message: discord.Message, args: list):
     if message.author.guild_permissions.manage_roles:
         empty_roles = list(filter(lambda r: len(r.members) == 0, message.guild.roles))
         [await role.delete() for role in empty_roles]
-        response = discord.Embed(color=0x77B255, title=f'Removed {len(empty_roles)} roles from this server.')
+        response = discord.Embed(color=0x77B255, title=f'✅ Removed {len(empty_roles)} roles from this server.')
         await message.channel.send(embed=response)
