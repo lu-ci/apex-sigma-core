8b0d3135348fdc2e3e53bf3f35fbb8bff118f72b
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-05-01 00:43:08 -0400

    Added a safeguard for missing arguments

--- sigma/modules/owner_controls/profile/setusername.py
+++ sigma/modules/owner_controls/profile/setusername.py
@@ -20,10 +20,13 @@ from sigma.core.mechanics.command import SigmaCommand
 
 
 async def setusername(cmd: SigmaCommand, message: discord.Message, args: list):
-    name_input = ' '.join(args)
-    try:
-        await cmd.bot.user.edit(username=name_input)
-        response = discord.Embed(color=0x77B255, title=f'✅ Changed username to {name_input}.')
-    except discord.Forbidden:
-        response = discord.Embed(color=0xBE1931, title=f'❗ I was unable to change my username.')
+    if args:
+        name_input = ' '.join(args)
+        try:
+            await cmd.bot.user.edit(username=name_input)
+            response = discord.Embed(color=0x77B255, title=f'✅ Changed username to {name_input}.')
+        except discord.Forbidden:
+            response = discord.Embed(color=0xBE1931, title=f'❗ I was unable to change my username.')
+    else:
+        response = discord.Embed(color=0xBE1931, title='❗ Nothing inputted.')
     await message.channel.send(embed=response)
