1b0609ece062fa79a290580db890b78e04c50388
AXAz0r <thealeksaradovic@gmail.com>
2018-06-06 12:12:41 +0200

    Status Rotation Hash Removal

--- sigma/modules/core_functions/playing_status/status_rotation.py
+++ sigma/modules/core_functions/playing_status/status_rotation.py
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import asyncio
-import hashlib
 import secrets
 
 import discord
@@ -24,18 +23,6 @@ from sigma.core.mechanics.event import SigmaEvent
 
 status_cache = []
 status_loop_running = False
-
-
-def random_capitalize(text):
-    new_text = ''
-    char_list = list(text)
-    while char_list:
-        char_choice = char_list.pop(0)
-        cap_roll = secrets.randbelow(2)
-        if cap_roll == 0:
-            char_choice = char_choice.upper()
-        new_text += char_choice
-    return new_text
 
 
 async def status_rotation(ev: SigmaEvent):
@@ -56,21 +43,12 @@ async def status_clockwork(ev: SigmaEvent):
                         status_cache.append(status_text)
                 if status_cache:
                     status = status_cache.pop(secrets.randbelow(len(status_cache)))
-                    mode_roll = secrets.randbelow(10)
-                    if mode_roll == 0:
-                        hgen = hashlib.new('md5')
-                        hgen.update(status.encode('utf-8'))
-                        digest = hgen.hexdigest()
-                        max_end = abs(len(digest) - 10)
-                        cut = secrets.randbelow(max_end)
-                        cut_text = digest[cut:(cut + 10)]
-                        status = random_capitalize(cut_text)
                     activity = discord.Activity(name=status, type=discord.ActivityType.playing)
                     try:
                         if ev.bot.cfg.pref.dev_mode:
                             await ev.bot.change_presence(activity=activity, status=discord.Status.dnd)
                         else:
                             await ev.bot.change_presence(activity=activity)
-                    except discord.ConnectionClosed:
+                    except Exception:
                         pass
         await asyncio.sleep(60)
