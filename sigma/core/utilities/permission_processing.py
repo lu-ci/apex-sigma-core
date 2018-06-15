1237943bfedced7888714c87a470b69ae680cad4
AXAz0r <thealeksaradovic@gmail.com>
2018-06-15 12:36:31 +0200

    Check for Owner in Hierarchy Permissions

--- sigma/core/utilities/permission_processing.py
+++ sigma/core/utilities/permission_processing.py
@@ -18,4 +18,5 @@
 def hierarchy_permit(author, target):
     top_author_role = author.top_role.position
     top_target_role = target.top_role.position
-    return top_author_role > top_target_role
+    target_is_owner = target.id == target.guild.owner.id
+    return top_author_role > top_target_role and not target_is_owner
