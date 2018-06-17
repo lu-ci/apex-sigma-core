e2f221e95ee9af0ef4a6bc80f2d0fb6a67a1ca0c
AXAz0r <thealeksaradovic@gmail.com>
2018-06-17 10:57:20 +0200

    Add Val's Dockertool

---
+++ dockertool.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "yaml"
+require "childprocess"
+
+INFO = YAML.load_file("info/version.yml").freeze
+CODENAME = INFO["codename"].downcase
+VERSION = INFO["version"]
+REGISTRY = "registry.gitlab.com"
+NAMESPACE = "lu-ci"
+IMAGE = "apex-sigma"
+
+def tagged(tag)
+  "#{REGISTRY}/#{NAMESPACE}/#{IMAGE}:#{tag}"
+end
+
+@tags = [
+  tagged(CODENAME),
+  tagged("#{VERSION['major']}.#{VERSION['minor']}.#{VERSION['patch']}"),
+  tagged("#{VERSION['major']}.#{VERSION['minor']}"),
+  tagged(VERSION['major']),
+  tagged("latest")
+].freeze
+
+def build
+  tags = @tags.flat_map { |t| ["--tag", t] }
+  cmd = ["docker", "build"] + tags << "."
+  ChildProcess.build(*cmd).tap do |p|
+    p.io.stdout = $stdout
+    p.io.stderr = $stderr
+    p.start
+    p.wait
+  end
+end
+
+def push
+  @tags.each do |tag|
+    ChildProcess.build("docker", "push", tag).tap do |p|
+      p.io.stdout = $stdout
+      p.io.stderr = $stderr
+      p.start
+      p.wait
+    end
+  end
+end
+
+def show
+  puts @tags
+end
+
+case ARGV.first
+when "build" then build
+when "push" then push
+when "show" then show
+end
