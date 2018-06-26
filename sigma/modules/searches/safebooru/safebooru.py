1469556a8ad3dd953bfb535fd454bd2e7f610180
AXAz0r <thealeksaradovic@gmail.com>
2018-06-27 00:15:44 +0200

    Shifty's Safebooru Cleaning

--- sigma/modules/searches/safebooru/safebooru.py
+++ sigma/modules/searches/safebooru/safebooru.py
@@ -24,28 +24,23 @@ from sigma.core.mechanics.command import SigmaCommand
 
 
 async def safebooru(cmd: SigmaCommand, message: discord.Message, args: list):
-    if not args:
-        tag = 'cute'
-    else:
-        tag = ' '.join(args)
-        tag = tag.replace(' ', '+')
-    resource = 'http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=' + tag
+    tag = ' '.join(args).replace(' ', '+') if args else 'cute'
+    resource = 'http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=rating:safe+' + tag
     async with aiohttp.ClientSession() as session:
         async with session.get(resource) as data:
             data = await data.read()
     posts = html.fromstring(data)
-    if len(posts) == 0:
-        embed = discord.Embed(color=0x696969, title='🔍 No results.')
-    else:
-        choice = secrets.choice(posts)
-        image_url = choice.attrib['file_url']
+    if posts:
+        post = secrets.choice(posts).attrib
+        image_url = post['file_url']
         icon_url = 'https://i.imgur.com/3Vb6LdJ.png'
-        post_url = f'http://safebooru.org/index.php?page=post&s=view&id={choice.attrib["id"]}'
+        post_url = f'http://safebooru.org/index.php?page=post&s=view&id={post["id"]}'
         if image_url.startswith('//'):
             image_url = 'http:' + image_url
-        embed = discord.Embed(color=0x8bb2a7)
-        embed.set_author(name='Safebooru', icon_url=icon_url, url=post_url)
-        embed.set_image(url=image_url)
-        embed.set_footer(
-            text=f'Score: {choice.attrib["score"]} | Size: {choice.attrib["width"]}x{choice.attrib["height"]}')
-    await message.channel.send(None, embed=embed)
+        response = discord.Embed(color=0x8bb2a7)
+        response.set_author(name='Safebooru', icon_url=icon_url, url=post_url)
+        response.set_image(url=image_url)
+        response.set_footer(text=f'Score: {post["score"]} | Size: {post["width"]}x{post["height"]}')
+    else:
+        response = discord.Embed(color=0x696969, title='🔍 No results.')
+    await message.channel.send(None, embed=response)
