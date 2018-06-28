863a2c94e3ba18821377ba092d4448327387e2a8
AXAz0r <thealeksaradovic@gmail.com>
2018-06-29 00:16:04 +0200

    Leaderboard DeCache

--- sigma/modules/statistics/leaderboards/topcookies.py
+++ sigma/modules/statistics/leaderboards/topcookies.py
@@ -14,22 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import arrow
 import discord
 from humanfriendly.tables import format_pretty_table as boop
 
-from sigma.core.mechanics.caching import Cacher
 from sigma.core.mechanics.command import SigmaCommand
 from sigma.modules.moderation.server_settings.filters.name_check_clockwork import clean_name
-
-
-tck_cache = Cacher(True, 3600)
 
 
 async def topcookies(cmd: SigmaCommand, message: discord.Message, args: list):
     value_name = 'Cookies'
     sort_key = 'Cookies'
-    cache_key = sort_key
     lb_category = 'Global'
     localed = False
     if args:
@@ -40,35 +34,28 @@ async def topcookies(cmd: SigmaCommand, message: discord.Message, args: list):
             lb_category = 'Local'
             localed = True
     coll = cmd.db[cmd.db.db_cfg.database].Cookies
+    all_docs = await coll.find({}).sort(sort_key, -1).limit(50).to_list(None)
+    leader_docs = []
     if localed:
-        cache_key = f'{sort_key}_local'
-    table_body = tck_cache.get_cache(cache_key)
-    if not table_body:
-        all_docs = await coll.find({}).sort(sort_key, -1).limit(50).to_list(None)
-        leader_docs = []
-        if localed:
-            all_members = message.guild.members
-        else:
-            all_members = cmd.bot.get_all_members()
-        for data_doc in all_docs:
-            user_value = data_doc.get(sort_key) or 0
-            user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
-            if user_object:
-                if user_value:
-                    leader_docs.append([user_object, user_value])
-                    if len(leader_docs) >= 20:
-                        break
-        table_data = [
-            [
-                pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
-                clean_name(doc[0].name, 'Unknown')[:12],
-                str(doc[1])
-            ] for pos, doc in enumerate(leader_docs)
-        ]
-        table_body = boop(table_data, ['#', 'User Name', value_name])
-        tck_cache.set_cache(cache_key, table_body)
-    last_updated = arrow.get(tck_cache.get_executed(sort_key))
+        all_members = message.guild.members
+    else:
+        all_members = cmd.bot.get_all_members()
+    for data_doc in all_docs:
+        user_value = data_doc.get(sort_key) or 0
+        user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
+        if user_object:
+            if user_value:
+                leader_docs.append([user_object, user_value])
+                if len(leader_docs) >= 20:
+                    break
+    table_data = [
+        [
+            pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
+            clean_name(doc[0].name, 'Unknown')[:12],
+            str(doc[1])
+        ] for pos, doc in enumerate(leader_docs)
+    ]
+    table_body = boop(table_data, ['#', 'User Name', value_name])
     response = f'**{lb_category} {value_name} Leaderboard**'
     response += f'\n```hs\n{table_body}\n```'
-    response += f'\nLeaderboard last updated {last_updated.humanize()}.'
     await message.channel.send(response)
--- sigma/modules/statistics/leaderboards/topcurrency.py
+++ sigma/modules/statistics/leaderboards/topcurrency.py
@@ -14,16 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import arrow
 import discord
 from humanfriendly.tables import format_pretty_table as boop
 
-from sigma.core.mechanics.caching import Cacher
 from sigma.core.mechanics.command import SigmaCommand
 from sigma.modules.moderation.server_settings.filters.name_check_clockwork import clean_name
-
-
-tcr_cache = Cacher(True, 3600)
 
 
 async def topcurrency(cmd: SigmaCommand, message: discord.Message, args: list):
@@ -40,33 +35,28 @@ async def topcurrency(cmd: SigmaCommand, message: discord.Message, args: list):
             sort_key = 'total'
             lb_category = 'Total'
     coll = cmd.db[cmd.db.db_cfg.database].CurrencySystem
-    table_body = tcr_cache.get_cache(sort_key)
-    if not table_body:
-        all_docs = await coll.find(search).sort(sort_key, -1).limit(50).to_list(None)
-        leader_docs = []
-        all_members = cmd.bot.get_all_members()
-        for data_doc in all_docs:
-            if sort_key == 'global' or sort_key == 'total':
-                user_value = data_doc.get(sort_key) or 0
-            else:
-                user_value = data_doc.get('guilds').get(str(message.guild.id)) or 0
-            user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
-            if user_object:
-                if user_value:
-                    leader_docs.append([user_object, user_value])
-                    if len(leader_docs) >= 20:
-                        break
-        table_data = [
-            [
-                pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
-                clean_name(doc[0].name, 'Unknown')[:12],
-                str(doc[1])
-            ] for pos, doc in enumerate(leader_docs)
-        ]
-        table_body = boop(table_data, ['#', 'User Name', value_name])
-        tcr_cache.set_cache(sort_key, table_body)
-    last_updated = arrow.get(tcr_cache.get_executed(sort_key))
+    all_docs = await coll.find(search).sort(sort_key, -1).limit(50).to_list(None)
+    leader_docs = []
+    all_members = cmd.bot.get_all_members()
+    for data_doc in all_docs:
+        if sort_key == 'global' or sort_key == 'total':
+            user_value = data_doc.get(sort_key) or 0
+        else:
+            user_value = data_doc.get('guilds').get(str(message.guild.id)) or 0
+        user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
+        if user_object:
+            if user_value:
+                leader_docs.append([user_object, user_value])
+                if len(leader_docs) >= 20:
+                    break
+    table_data = [
+        [
+            pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
+            clean_name(doc[0].name, 'Unknown')[:12],
+            str(doc[1])
+        ] for pos, doc in enumerate(leader_docs)
+    ]
+    table_body = boop(table_data, ['#', 'User Name', value_name])
     response = f'**{lb_category} {value_name} Leaderboard**'
     response += f'\n```hs\n{table_body}\n```'
-    response += f'\nLeaderboard last updated {last_updated.humanize()}.'
     await message.channel.send(response)
