dd7aeb12b980998b08b65ac9c46c8969503930fb
AXAz0r <thealeksaradovic@gmail.com>
2018-07-01 13:57:12 +0200

    Leaderboard Cache and Passive EXP

--- sigma/modules/statistics/leaderboards/topcookies.py
+++ sigma/modules/statistics/leaderboards/topcookies.py
@@ -14,11 +14,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import arrow
 import discord
 from humanfriendly.tables import format_pretty_table as boop
 
+from sigma.core.mechanics.caching import Cacher
 from sigma.core.mechanics.command import SigmaCommand
 from sigma.modules.moderation.server_settings.filters.name_check_clockwork import clean_name
+
+tcklb_cache = Cacher()
 
 
 async def topcookies(cmd: SigmaCommand, message: discord.Message, args: list):
@@ -33,21 +37,27 @@ async def topcookies(cmd: SigmaCommand, message: discord.Message, args: list):
         elif args[0].lower() == 'local':
             lb_category = 'Local'
             localed = True
-    coll = cmd.db[cmd.db.db_cfg.database].Cookies
-    all_docs = await coll.find({}).sort(sort_key, -1).limit(50).to_list(None)
-    leader_docs = []
-    if localed:
-        all_members = message.guild.members
-    else:
-        all_members = cmd.bot.get_all_members()
-    for data_doc in all_docs:
-        user_value = data_doc.get(sort_key) or 0
-        user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
-        if user_object:
-            if user_value:
-                leader_docs.append([user_object, user_value])
-                if len(leader_docs) >= 20:
-                    break
+    cache_key = message.guild.id if localed else sort_key
+    now = arrow.utcnow().timestamp
+    leader_docs, leader_timer = tcklb_cache.get_cache(cache_key), tcklb_cache.get_cache(f'{cache_key}_stamp') or now
+    if not leader_docs or leader_timer < now + 180:
+        coll = cmd.db[cmd.db.db_cfg.database].Cookies
+        all_docs = await coll.find({}).sort(sort_key, -1).limit(50).to_list(None)
+        leader_docs = []
+        if localed:
+            all_members = message.guild.members
+        else:
+            all_members = cmd.bot.get_all_members()
+        for data_doc in all_docs:
+            user_value = data_doc.get(sort_key) or 0
+            user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
+            if user_object:
+                if user_value:
+                    leader_docs.append([user_object, user_value])
+                    if len(leader_docs) >= 20:
+                        break
+        tcklb_cache.set_cache(cache_key, leader_docs)
+        tcklb_cache.set_cache(f'{cache_key}_stamp', now)
     table_data = [
         [
             pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
@@ -56,6 +66,7 @@ async def topcookies(cmd: SigmaCommand, message: discord.Message, args: list):
         ] for pos, doc in enumerate(leader_docs)
     ]
     table_body = boop(table_data, ['#', 'User Name', value_name])
-    response = f'**{lb_category} {value_name} Leaderboard**'
+    response = f'🍪 **{lb_category} {value_name} Leaderboard**'
     response += f'\n```hs\n{table_body}\n```'
+    response += f'\nLeaderboard last updated {arrow.get(leader_timer).humanize()}.'
     await message.channel.send(response)
--- sigma/modules/statistics/leaderboards/topexperience.py
+++ sigma/modules/statistics/leaderboards/topexperience.py
@@ -13,12 +13,15 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import arrow
 import discord
 from humanfriendly.tables import format_pretty_table as boop
 
+from sigma.core.mechanics.caching import Cacher
 from sigma.core.mechanics.command import SigmaCommand
 from sigma.modules.moderation.server_settings.filters.name_check_clockwork import clean_name
+
+txplb_cache = Cacher()
 
 
 async def topexperience(cmd: SigmaCommand, message: discord.Message, args: list):
@@ -34,22 +37,27 @@ async def topexperience(cmd: SigmaCommand, message: discord.Message, args: list)
         elif args[0].lower() == 'total':
             sort_key = 'total'
             lb_category = 'Total'
-    coll = cmd.db[cmd.db.db_cfg.database].ExperienceSystem
-    all_docs = await coll.find(search).sort(sort_key, -1).limit(50).to_list(None)
-    leader_docs = []
-    all_members = cmd.bot.get_all_members()
-    for data_doc in all_docs:
-        if sort_key == 'global' or sort_key == 'total':
-            user_value = data_doc.get(sort_key) or 0
-        else:
-            user_value = data_doc.get('guilds').get(str(message.guild.id)) or 0
-        user_level = int(user_value / 13266.85)
-        user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
-        if user_object:
-            if user_value:
-                leader_docs.append([user_object, user_level, user_value])
-                if len(leader_docs) >= 20:
-                    break
+    now = arrow.utcnow().timestamp
+    leader_docs, leader_timer = txplb_cache.get_cache(sort_key), txplb_cache.get_cache(f'{sort_key}_stamp') or now
+    if not leader_docs or leader_timer < now + 180:
+        coll = cmd.db[cmd.db.db_cfg.database].ExperienceSystem
+        all_docs = await coll.find(search).sort(sort_key, -1).limit(50).to_list(None)
+        leader_docs = []
+        all_members = cmd.bot.get_all_members()
+        for data_doc in all_docs:
+            if sort_key == 'global' or sort_key == 'total':
+                user_value = data_doc.get(sort_key) or 0
+            else:
+                user_value = data_doc.get('guilds').get(str(message.guild.id)) or 0
+            user_level = int(user_value / 13266.85)
+            user_object = discord.utils.find(lambda usr: usr.id == data_doc.get('UserID'), all_members)
+            if user_object:
+                if user_value:
+                    leader_docs.append([user_object, user_level, user_value])
+                    if len(leader_docs) >= 20:
+                        break
+        txplb_cache.set_cache(sort_key, leader_docs)
+        txplb_cache.set_cache(f'{sort_key}_stamp', now)
     table_data = [
         [
             pos + 1 if not doc[0].id == message.author.id else f'{pos + 1} <',
@@ -59,6 +67,7 @@ async def topexperience(cmd: SigmaCommand, message: discord.Message, args: list)
         ] for pos, doc in enumerate(leader_docs)
     ]
     table_body = boop(table_data, ['#', 'User Name', 'Level', value_name])
-    response = f'**{lb_category} {value_name} Leaderboard**'
+    response = f'🔰 **{lb_category} {value_name} Leaderboard**'
     response += f'\n```hs\n{table_body}\n```'
+    response += f'\nLeaderboard last updated {arrow.get(leader_timer).humanize()}.'
     await message.channel.send(response)
