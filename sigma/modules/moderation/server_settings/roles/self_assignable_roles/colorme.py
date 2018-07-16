063b712fdf3ba2a58e70b3926e9bfb517e09a0fe
AXAz0r <thealeksaradovic@gmail.com>
2018-07-16 15:16:34 +0200

    Colorme Position Safeguard

--- sigma/modules/moderation/server_settings/roles/self_assignable_roles/colorme.py
+++ sigma/modules/moderation/server_settings/roles/self_assignable_roles/colorme.py
@@ -21,34 +21,37 @@ from sigma.core.mechanics.command import SigmaCommand
 
 
 async def colorme(cmd: SigmaCommand, message: discord.Message, args: list):
-    enabled = await cmd.db.get_guild_settings(message.guild.id, 'ColorRoles')
-    if enabled:
-        if args:
-            bad_hex = False
-            hex_req = args[0].lower().strip('#')
-            if len(hex_req) == 3:
-                hex_req = hex_req * 2
-            if len(hex_req) != 6:
-                bad_hex = True
-            if not bad_hex:
-                role_name = f'SCR-{hex_req.upper()}'
-                role_posi = message.guild.me.top_role.position + 1
-                role_objc = discord.utils.find(lambda role: role.name == role_name, message.guild.roles)
-                if not role_objc:
-                    color_int = int(f'0x{hex_req}', 16)
-                    role_color = discord.Color(color_int)
-                    role_objc = await message.guild.create_role(name=role_name, color=role_color)
-                    await role_objc.edit(position=role_posi)
-                for member_role in message.author.roles:
-                    if member_role.name.startswith('SCR-'):
-                        await message.author.remove_roles(member_role, reason='Assigning new color role.')
-                await message.author.add_roles(role_objc, reason='Assigned color role.')
-                addition_title = f'✅ {role_objc.name} has been added to you, {message.author.name}.'
-                response = discord.Embed(color=0x77B255, title=addition_title)
+    if not message.guild.me.top_role.position == len(message.guild.roles):
+        enabled = await cmd.db.get_guild_settings(message.guild.id, 'ColorRoles')
+        if enabled:
+            if args:
+                bad_hex = False
+                hex_req = args[0].lower().strip('#')
+                if len(hex_req) == 3:
+                    hex_req = hex_req * 2
+                if len(hex_req) != 6:
+                    bad_hex = True
+                if not bad_hex:
+                    role_name = f'SCR-{hex_req.upper()}'
+                    role_posi = message.guild.me.top_role.position + 1
+                    role_objc = discord.utils.find(lambda role: role.name == role_name, message.guild.roles)
+                    if not role_objc:
+                        color_int = int(f'0x{hex_req}', 16)
+                        role_color = discord.Color(color_int)
+                        role_objc = await message.guild.create_role(name=role_name, color=role_color)
+                        await role_objc.edit(position=role_posi)
+                    for member_role in message.author.roles:
+                        if member_role.name.startswith('SCR-'):
+                            await message.author.remove_roles(member_role, reason='Assigning new color role.')
+                    await message.author.add_roles(role_objc, reason='Assigned color role.')
+                    addition_title = f'✅ {role_objc.name} has been added to you, {message.author.name}.'
+                    response = discord.Embed(color=0x77B255, title=addition_title)
+                else:
+                    response = discord.Embed(color=0xBE1931, title='❗ Invalid HEX color code.')
             else:
-                response = discord.Embed(color=0xBE1931, title='❗ Invalid HEX color code.')
+                response = discord.Embed(color=0xBE1931, title='❗ No color HEX provided.')
         else:
-            response = discord.Embed(color=0xBE1931, title='❗ No color HEX provided.')
+            response = discord.Embed(color=0xBE1931, title=f'⛔ Color roles are not enabled.')
     else:
-        response = discord.Embed(color=0xBE1931, title=f'⛔ Color roles are not enabled.')
+        response = discord.Embed(color=0xBE1931, title='❗ I can\'t make a color role with my current role position.')
     await message.channel.send(embed=response)
