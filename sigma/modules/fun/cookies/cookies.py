cd9fb77b2237b9adc1ca3969a8f4bf51aebf7361
AXAz0r <thealeksaradovic@gmail.com>
2018-07-17 11:56:43 +0200

    Add Total Cookie Display

--- sigma/modules/fun/cookies/cookies.py
+++ sigma/modules/fun/cookies/cookies.py
@@ -23,7 +23,9 @@ async def cookies(cmd: SigmaCommand, message: discord.Message, args: list):
     target = message.author if not message.mentions else message.mentions[0]
     cookie_file = await cmd.db[cmd.db.db_nam].Cookies.find_one({'UserID': target.id}) or {}
     cookie_count = cookie_file.get('Cookies') or 0
+    cookie_total = cookie_file.get('Total') or 0
     ender = 'cookie' if cookie_count == 1 else 'cookies'
-    title = f'🍪 {target.display_name} has {cookie_count} {ender}.'
+    title = f'🍪 {target.display_name} got {cookie_count} {ender} this month '
+    title += f'and has {cookie_total} in total.'
     response = discord.Embed(color=0xd99e82, title=title)
     await message.channel.send(embed=response)
