ee350a592948b97a424f8e7ec7a2b45ee7c037f1
AXAz0r <thealeksaradovic@gmail.com>
2018-09-02 20:14:17 +0200

    Item Recipe Presence

--- sigma/modules/minigames/professions/nodes/recipe_core.py
+++ sigma/modules/minigames/professions/nodes/recipe_core.py
@@ -13,6 +13,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from sigma.core.mechanics.database import Database
 from sigma.modules.minigames.professions.nodes.item_core import get_item_core
 from sigma.modules.minigames.professions.nodes.properties import cook_icons, cook_colors
