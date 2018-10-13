b8b1d4196443be32df12907ce34b61c52a1b190f
AXAz0r <thealeksaradovic@gmail.com>
2018-10-13 19:59:31 +0200

    Race Sabotage Safeguard

--- sigma/modules/minigames/racing/joinrace.py
+++ sigma/modules/minigames/racing/joinrace.py
@@ -27,27 +27,30 @@ async def joinrace(cmd: SigmaCommand, message: discord.Message, args: list):
         buyin = race['buyin']
         kud = await cmd.db.get_resource(message.author.id, 'currency')
         kud = kud.current
-        if kud >= buyin:
-            if len(race['users']) < 10:
-                user_found = False
-                for user in race['users']:
-                    if user['user'].id == message.author.id:
-                        user_found = True
-                        break
-                if not user_found:
-                    icon = add_participant(message.channel.id, message.author)
-                    if names[icon][0] in ['a', 'e', 'i', 'o', 'u']:
-                        connector = 'an'
+        if not await cmd.db.is_sabotaged(message.author.id):
+            if kud >= buyin:
+                if len(race['users']) < 10:
+                    user_found = False
+                    for user in race['users']:
+                        if user['user'].id == message.author.id:
+                            user_found = True
+                            break
+                    if not user_found:
+                        icon = add_participant(message.channel.id, message.author)
+                        if names[icon][0] in ['a', 'e', 'i', 'o', 'u']:
+                            connector = 'an'
+                        else:
+                            connector = 'a'
+                        join_title = f'{icon} {message.author.display_name} joined as {connector} {names[icon]}!'
+                        response = discord.Embed(color=colors[icon], title=join_title)
                     else:
-                        connector = 'a'
-                    join_title = f'{icon} {message.author.display_name} joined as {connector} {names[icon]}!'
-                    response = discord.Embed(color=colors[icon], title=join_title)
+                        response = discord.Embed(color=0xBE1931, title='❗ You are already in the race!')
                 else:
-                    response = discord.Embed(color=0xBE1931, title='❗ You are already in the race!')
+                    response = discord.Embed(color=0xBE1931, title='❗ Sorry, no more room left!')
             else:
-                response = discord.Embed(color=0xBE1931, title='❗ Sorry, no more room left!')
+                response = discord.Embed(color=0xBE1931, title=f'❗ You don\'t have that much {currency}!')
         else:
-            response = discord.Embed(color=0xBE1931, title=f'❗ You don\'t have that much {currency}!')
+            response = discord.Embed(color=0xBE1931, title='❗ We failed to sign you up for the race.')
     else:
         response = discord.Embed(color=0xBE1931, title='❗ There is no race in preparation.')
     await message.channel.send(embed=response)
