7a21c7a231688d6bf839aa201e2f3b7d3680c4c2
AXAz0r <thealeksaradovic@gmail.com>
2018-10-20 12:33:31 +0200

    Wiper Commands

---
+++ sigma/modules/owner_controls/resources/wiperesources.py
@@ -0,0 +1,37 @@
+# Apex Sigma: The Database Giant Discord Bot.
+# Copyright (C) 2018  Lucia's Cipher
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import discord
+
+from sigma.core.mechanics.command import SigmaCommand
+from sigma.core.mechanics.resources import SigmaResource
+
+
+async def wiperesources(cmd: SigmaCommand, message: discord.Message, args: list):
+    try:
+        target = cmd.bot.get_user(int(args[0])) if args else None
+    except ValueError:
+        target = None
+    if target:
+        colls = await cmd.db[cmd.db.db_nam].list_collection_names()
+        reses = list(sorted([coll[:-8].lower() for coll in colls if coll.endswith('Resource')]))
+        for res in reses:
+            new_res = SigmaResource({})
+            await cmd.db.update_resource(target.id, res, new_res)
+        response = discord.Embed(color=0xFFCC4D, title=f'🔥 Ok, I\'ve wiped {target.display_name}\'s resources.')
+    else:
+        response = discord.Embed(color=0x696969, title='🔍 User not found.')
+    await message.channel.send(embed=response)
