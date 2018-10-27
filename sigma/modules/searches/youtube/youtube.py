474964b52ea527840ab6671125c1e96d1d47f5a6
AXAz0r <thealeksaradovic@gmail.com>
2018-10-27 22:08:22 +0200

    Y Function Payload Rework

--- sigma/modules/searches/youtube/youtube.py
+++ sigma/modules/searches/youtube/youtube.py
@@ -19,9 +19,11 @@ import datetime
 import discord
 
 from sigma.core.mechanics.command import SigmaCommand
+from sigma.core.mechanics.payload import CommandPayload
 
 
 async def youtube(cmd: SigmaCommand, pld: CommandPayload):
+    message, args = pld.msg, pld.args
     yt_icon = 'https://i.imgur.com/qoH1MUP.png'
     yt_color = 0xcf2227
     text_mode = False
