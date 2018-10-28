2690455b0d98bfdc243c7accdcb72598cbc48923
AXAz0r <thealeksaradovic@gmail.com>
2018-10-28 10:44:40 +0100

    Starboard Check Fix

--- sigma/modules/moderation/server_settings/starboard/starboard_watcher.py
+++ sigma/modules/moderation/server_settings/starboard/starboard_watcher.py
@@ -89,7 +89,7 @@ async def starboard_watcher(ev: SigmaEvent, pld: RawReactionPayload):
                                 if user:
                                     if not user.bot:
                                         try:
-                                            enough = check_emotes(ev.db, mid, sbl)
+                                            enough = await check_emotes(ev.db, mid, sbl)
                                             if enough:
                                                 message = await channel.get_message(mid)
                                                 if not message.author.bot:
