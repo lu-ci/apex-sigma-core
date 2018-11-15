d203417bf1c1f7f7989761328362b96e29c786dc
AXAz0r <thealeksaradovic@gmail.com>
2018-11-15 13:11:09 +0100

    Website Replacements

--- sigma/modules/help/donate.py
+++ sigma/modules/help/donate.py
@@ -23,7 +23,7 @@ from sigma.core.mechanics.payload import CommandPayload
 async def donate(cmd: SigmaCommand, pld: CommandPayload):
     message = pld.msg
     sigma_title = 'Sigma Donation Information'
-    donation_url = f'{cmd.bot.cfg.pref.website}/donate'
+    donation_url = cmd.bot.cfg.pref.website
     response = discord.Embed(color=0x1B6F5F, title=sigma_title)
     response.description = f'Care to help out? Come [support]({donation_url}) Sigma!'
     await message.channel.send(embed=response)
