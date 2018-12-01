52959a7888eed12676b4c7b8ffcc351f346908a1
AXAz0r <thealeksaradovic@gmail.com>
2018-12-01 21:21:49 +0100

    Faster Leaderboard Clear and Selective Leaderboard Awarding

--- sigma/modules/owner_controls/leaderboards/awardleaderboards.py
+++ sigma/modules/owner_controls/leaderboards/awardleaderboards.py
@@ -21,21 +21,11 @@ from sigma.core.mechanics.payload import CommandPayload
 from sigma.modules.statistics.leaderboards.topcookies import get_leader_docs
 
 
-async def reset_all(db, coll, nam):
-    docs = await coll.find({}).to_list(None)
-    for doc in docs:
-        uid = doc.get('user_id')
-        if uid:
-            resc = await db.get_resource(uid, nam)
-            resc.ranked = 0
-            await db.update_resource(uid, nam, resc)
-
-
 async def awardleaderboards(cmd: SigmaCommand, pld: CommandPayload):
-    awardables = ['currency', 'cookies']
-    init_resp = discord.Embed(color=0xf9f9f9, title=f'💴 Awarding leaderboards....')
-    init_msg = await pld.msg.channel.send(embed=init_resp)
-    for awdbl in awardables:
+    awdbl = pld.args[0] if pld.args else None
+    if awdbl:
+        init_resp = discord.Embed(color=0xf9f9f9, title=f'💴 Awarding leaderboards....')
+        init_msg = await pld.msg.channel.send(embed=init_resp)
         coll = cmd.db[cmd.db.db_nam][f'{awdbl.title()}Resource']
         search = {'$and': [{'ranked': {'$exists': True}}, {'ranked': {'$gt': 0}}]}
         all_docs = await coll.find(search).sort('ranked', -1).limit(100).to_list(None)
@@ -46,7 +36,9 @@ async def awardleaderboards(cmd: SigmaCommand, pld: CommandPayload):
             await cmd.db.add_resource(ld_entry[0], 'currency', ld_award, 'leaderboard', pld.msg, False)
             value = f'{ld_entry[1]} {awdbl.title()}'
             cmd.log.info(f'PLC: {20 - ld_index} | AMT: {ld_award} | USR: {ld_entry[0]} | VAL: {value}')
-        await reset_all(cmd.db, coll, awdbl)
-    await init_msg.delete()
-    done_resp = discord.Embed(color=0x77B255, title=f'✅ All leaderboards awarded.')
+        await coll.update_many({}, {'$set': {'ranked': 0}})
+        await init_msg.delete()
+        done_resp = discord.Embed(color=0x77B255, title=f'✅ All leaderboards awarded.')
+    else:
+        done_resp = discord.Embed(color=0xBE1931, title=f'❗ Nothing inputted.')
     await pld.msg.channel.send(embed=done_resp)
