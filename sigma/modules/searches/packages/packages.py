ed9fc85d4f05c89df6496183fb476cb62028ae66
Daniel Preston <prestondj.2001@yahoo.com>
2019-02-06 13:44:02 +0000

    Packages Command Fixes

--- sigma/modules/searches/packages/packages.py
+++ sigma/modules/searches/packages/packages.py
@@ -28,8 +28,8 @@ pypi_io_icon = 'https://i.imgur.com/BSUf5p2.png'
 
 
 async def packages(_cmd: SigmaCommand, pld: CommandPayload):
-    lookup = '_'.join(pld.args) if pld.args else None
-    if lookup:
+    if pld.args:
+        lookup = '_'.join(pld.args)
         package_url = f'https://pypi.org/pypi/{lookup}/json'
         try:
             package_data = await aioget(package_url, True)
@@ -38,22 +38,26 @@ async def packages(_cmd: SigmaCommand, pld: CommandPayload):
         if package_data:
             cdat = package_data.get('info')
             vers = package_data.get('releases')
-            versions = [v for v in sorted(vers.items(), key=lambda x: arrow.get(x[1][0].get('upload_time')).timestamp)]
-            created_at = arrow.get(versions[0][1][0].get('upload_time'))
-            updated_at = arrow.get(versions[-1][1][0].get('upload_time'))
-            package_page = cdat.get('package_url')
-            package_title = f'{cdat.get("name")} {versions[-1][0]}'
-            response = discord.Embed(color=0x3476a9, timestamp=updated_at.datetime)
-            response.description = f'Author: {cdat.get("author", "Unknown")}'
-            response.description += f'\nCreated: {created_at.format("DD. MMM. YYYY")}'
-            response.description += f'\n**{cdat.get("summary")}**'
-            response.set_author(name=package_title, icon_url=pypi_io_icon, url=package_page)
-            if cdat.get('keywords'):
-                response.set_footer(text=f'Keywords: {", ".join(cdat.get("keywords").split())}')
+            versions = filter(lambda v: v[1], vers.items())
+            versions = sorted(versions, key=lambda x: arrow.get(x[1][0].get('upload_time')).timestamp)
+            if versions:
+                created_at = arrow.get(versions[0][1][0].get('upload_time'))
+                updated_at = arrow.get(versions[-1][1][0].get('upload_time'))
+                package_page = cdat.get('package_url')
+                package_title = f'{cdat.get("name")} {versions[-1][0]}'
+                response = discord.Embed(color=0x3476a9, timestamp=updated_at.datetime)
+                response.description = f'Author: {cdat.get("author") or "Unknown"}'
+                response.description += f'\nCreated: {created_at.format("DD. MMM. YYYY")}'
+                response.description += f'\n**{cdat.get("summary") or "No summary provided."}**'
+                response.set_author(name=package_title, icon_url=pypi_io_icon, url=package_page)
+                if cdat.get('keywords'):
+                    response.set_footer(text=f'Keywords: {", ".join(cdat.get("keywords").split())}')
+                else:
+                    response.set_footer(text='Last updated')
             else:
-                response.set_footer(text='Last updated')
+                response = error('Insufficient data on that package.')
         else:
             response = not_found('Package not found.')
     else:
-        response = error('Nothing inputed.')
+        response = error('Nothing inputted.')
     await pld.msg.channel.send(embed=response)
