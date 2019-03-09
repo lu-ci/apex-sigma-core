7ff98ee3032d3421d1e0ab8c35976a70639d1a97
Daniel Preston <prestondj.2001@yahoo.com>
2019-03-09 05:27:35 +0000

    Error Reporter Fix

--- sigma/modules/core_functions/reporters/error_reporter.py
+++ sigma/modules/core_functions/reporters/error_reporter.py
@@ -44,7 +44,7 @@ async def send_error_log_message(bot: ApexSigma, error_data):
     await get_error_channel(bot)
     if error_channel and error_data:
         response, trace = SigmaError.make_error_embed(error_data)
-        await error_channel.send(tradcembed=response)
+        await error_channel.send(embed=response)
         if trace:
             await error_channel.send(trace)
 
