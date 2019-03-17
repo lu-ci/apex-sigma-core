21b14941c8a28abf1dda8b85a3423b1ec0259a0d
AXAz0r <thealeksaradovic@gmail.com>
2019-03-17 16:17:08 +0100

    Error Reporter Length Safeguard

--- sigma/modules/core_functions/reporters/error_reporter.py
+++ sigma/modules/core_functions/reporters/error_reporter.py
@@ -46,7 +46,7 @@ async def send_error_log_message(bot: ApexSigma, error_data):
         response, trace = SigmaError.make_error_embed(error_data)
         await error_channel.send(embed=response)
         if trace:
-            await error_channel.send(trace)
+            await error_channel.send(trace[:900])
 
 
 async def error_reporter_clockwork(ev: SigmaEvent):
