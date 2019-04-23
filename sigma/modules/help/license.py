e10b1a84782d7768354845692e8a7abca2f8a30b
AXAz0r <thealeksaradovic@gmail.com>
2019-04-23 16:57:48 +0200

    Intergritiy Check Base

---
+++ sigma/modules/help/license.py
@@ -0,0 +1,36 @@
+"""
+Apex Sigma: The Database Giant Discord Bot.
+Copyright (C) 2019  Lucia's Cipher
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+---------- IMPORTANT ----------
+You may not modify this command file
+under any circumstance as it delivers
+the details behind the software's license
+in a readable form to any end-user as
+well as any licensor information.
+-------------------------------
+"""
+
+
+async def showlicense(cmd, pld):
+    """
+    :param cmd: The command object referenced in the command.
+    :type cmd: sigma.core.mechanics.command.SigmaCommand
+    :param pld: The payload with execution data and details.
+    :type pld: sigma.core.mechanics.payload.CommandPayload
+    """
