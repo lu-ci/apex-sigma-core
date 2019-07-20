5e4cc9430c7f9fe709a619f809bb1d9377cd5fbf
Aleksa Radovic <thealeksaradovic@gmail.com>
2019-07-21 01:39:50 +0200

    Fix Virginity Giving

--- sigma/modules/utilities/marriage/givevirginity.py
+++ sigma/modules/utilities/marriage/givevirginity.py
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import copy
+
 import discord
 
 from sigma.core.utilities.dialogue_controls import bool_dialogue
@@ -29,7 +31,7 @@ class UserVirginity(object):
         self.empty = raw is None
         self.user_id = self.raw.get('user_id')
         self.first = self.raw.get('first')
-        self.virgin = self.raw.get('virgin') or True
+        self.virgin = True if self.raw.get('virgin') is None else self.raw.get('virgin')
         self.virginities = self.raw.get('virginities') or []
 
     def to_dict(self):
@@ -39,19 +41,6 @@ class UserVirginity(object):
             'virgin': self.virgin,
             'virginities': self.virginities
         }
-
-    async def save(self, db):
-        """
-        :param db: The database instance.
-        :type db: sigma.core.mechanics.database.Database
-        :return:
-        :rtype:
-        """
-        data = self.to_dict()
-        if self.empty:
-            await db[db.db_nam].Virginity.insert_one(data)
-        else:
-            await db[db.db_nam].Virginity.update_one({'user_id': self.user_id}, {'$set': data})
 
 
 async def givevirginity(cmd, pld):
@@ -63,30 +52,34 @@ async def givevirginity(cmd, pld):
     """
     if pld.msg.mentions:
         target = pld.msg.mentions[0]
-        av = UserVirginity(await cmd.db[cmd.db.db_name].Virginity.find_one({'user_id': pld.msg.author.id}))
+        adoc = await cmd.db.get_profile(pld.msg.author.id, 'virginity')
+        av = UserVirginity(adoc)
         if av.empty:
             av.user_id = pld.msg.author.id
         if av.virgin:
-            tv = UserVirginity(await cmd.db[cmd.db.db_name].Virginity.find_one({'user_id': target.id}))
+            tdoc = await cmd.db.get_profile(target.id, 'virginity')
+            tv = UserVirginity(tdoc)
             if tv.empty:
                 tv.user_id = target.id
             question = f'Do you accept {pld.msg.author.name}\'s virginity?'
             if tv.virgin:
                 question = f'Do you accept {pld.msg.author.name}\'s virginity in exchange for your own?'
             question = discord.Embed(color=0xF9F9F9, title=f'❔ {question}')
-            success, timeout = await bool_dialogue(cmd.bot, pld.msg, question)
+            fake_msg = copy.copy(pld.msg)
+            fake_msg.author = target
+            success, timeout = await bool_dialogue(cmd.bot, fake_msg, question)
             if success:
                 av.virgin = False
                 av.first = target.id
                 tv.virginities.append(pld.msg.author.id)
-                congrats_title = f'🎉 Congrats, you are no longer a child! ... apparently.'
+                congrats_title = f'🎉 Congrats, you are no longer a child!'
                 if tv.virgin:
                     tv.virgin = False
                     tv.first = pld.msg.author.id
                     av.virginities.append(target.id)
-                    congrats_title = f'🎉 Congrats, you are no longer children! ... apparently.'
-                await av.save(cmd.db)
-                await tv.save(cmd.db)
+                    congrats_title = f'🎉 Congrats, you are no longer children!'
+                await cmd.db.set_profile(pld.msg.author.id, 'virginity', av.to_dict())
+                await cmd.db.set_profile(target.id, 'virginity', tv.to_dict())
                 response = discord.Embed(color=0x66CC66, title=congrats_title)
             else:
                 if timeout:
