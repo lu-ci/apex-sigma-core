7dd462bf54977c52a4e70b245ddbc211ffcf492f
Aleksa Radovic <thealeksaradovic@gmail.com>
2019-07-21 15:23:08 +0200

    Give Virginity Tweaks

--- sigma/modules/utilities/marriage/givevirginity.py
+++ sigma/modules/utilities/marriage/givevirginity.py
@@ -52,42 +52,45 @@ async def givevirginity(cmd, pld):
     """
     if pld.msg.mentions:
         target = pld.msg.mentions[0]
-        adoc = await cmd.db.get_profile(pld.msg.author.id, 'virginity')
-        av = UserVirginity(adoc)
-        if av.empty:
-            av.user_id = pld.msg.author.id
-        if av.virgin:
-            tdoc = await cmd.db.get_profile(target.id, 'virginity')
-            tv = UserVirginity(tdoc)
-            if tv.empty:
-                tv.user_id = target.id
-            question = f'Do you accept {pld.msg.author.name}\'s virginity?'
-            if tv.virgin:
-                question = f'Do you accept {pld.msg.author.name}\'s virginity in exchange for your own?'
-            question = discord.Embed(color=0xF9F9F9, title=f'❔ {question}')
-            fake_msg = copy.copy(pld.msg)
-            fake_msg.author = target
-            success, timeout = await bool_dialogue(cmd.bot, fake_msg, question)
-            if success:
-                av.virgin = False
-                av.first = target.id
-                tv.virginities.append(pld.msg.author.id)
-                congrats_title = f'🎉 Congrats, you are no longer a child!'
+        if pld.msg.author.id != target.id:
+            adoc = await cmd.db.get_profile(pld.msg.author.id, 'virginity')
+            av = UserVirginity(adoc)
+            if av.empty:
+                av.user_id = pld.msg.author.id
+            if av.virgin:
+                tdoc = await cmd.db.get_profile(target.id, 'virginity')
+                tv = UserVirginity(tdoc)
+                if tv.empty:
+                    tv.user_id = target.id
+                question = f'Do you accept {pld.msg.author.display_name}\'s virginity?'
                 if tv.virgin:
-                    tv.virgin = False
-                    tv.first = pld.msg.author.id
-                    av.virginities.append(target.id)
-                    congrats_title = f'🎉 Congrats, you are no longer children!'
-                await cmd.db.set_profile(pld.msg.author.id, 'virginity', av.to_dict())
-                await cmd.db.set_profile(target.id, 'virginity', tv.to_dict())
-                response = discord.Embed(color=0x66CC66, title=congrats_title)
-            else:
-                if timeout:
-                    response = discord.Embed(color=0x696969, title=f'🕙 {target.name} didn\'t respond.')
+                    question = f'Do you accept {pld.msg.author.display_name}\'s virginity in exchange for your own?'
+                question = discord.Embed(color=0xF9F9F9, title=f'❔ {question}')
+                fake_msg = copy.copy(pld.msg)
+                fake_msg.author = target
+                success, timeout = await bool_dialogue(cmd.bot, fake_msg, question)
+                if success:
+                    av.virgin = False
+                    av.first = target.id
+                    tv.virginities.append(pld.msg.author.id)
+                    congrats_title = f'🎉 Congrats, you are no longer a child!'
+                    if tv.virgin:
+                        tv.virgin = False
+                        tv.first = pld.msg.author.id
+                        av.virginities.append(target.id)
+                        congrats_title = f'🎉 Congrats, you are no longer children!'
+                    await cmd.db.set_profile(pld.msg.author.id, 'virginity', av.to_dict())
+                    await cmd.db.set_profile(target.id, 'virginity', tv.to_dict())
+                    response = discord.Embed(color=0x66CC66, title=congrats_title)
                 else:
-                    response = discord.Embed(color=0xBE1931, title=f'❌ {target.name} rejected you.')
+                    if timeout:
+                        response = discord.Embed(color=0x696969, title=f'🕙 {target.display_name} didn\'t respond.')
+                    else:
+                        response = discord.Embed(color=0xBE1931, title=f'❌ {target.display_name} rejected you.')
+            else:
+                response = error('You are not a virgin.')
         else:
-            response = error('You are not a virgin.')
+            response = error(f'Now that\'s just sad, {pld.msg.author.display_name}...')
     else:
         response = error('No user mentioned.')
     await pld.msg.channel.send(embed=response)
