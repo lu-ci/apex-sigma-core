9bde109b782b14f0ad4bfc605fa473476dd8cf67
Aleksa Radovic <thealeksaradovic@gmail.com>
2019-09-01 10:16:00 +0200

    Add The Entire Missing Tackle Command

---
+++ sigma/modules/interactions/tackle.py
@@ -0,0 +1,37 @@
+"""
+Apex Sigma: The Database Giant Discord Bot.
+Copyright (C) 2019  Lucia's Cipher
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import discord
+
+from sigma.modules.interactions.mech.interaction_mechanics import grab_interaction, get_target, make_footer, get_author
+
+
+async def tackle(cmd, pld):
+    """
+    :param cmd: The command object referenced in the command.
+    :type cmd: sigma.core.mechanics.command.SigmaCommand
+    :param pld: The payload with execution data and details.
+    :type pld: sigma.core.mechanics.payload.CommandPayload
+    """
+    interaction = await grab_interaction(cmd.db, 'tackle')
+    target, auth = get_target(pld.msg.guild.me, pld.msg), get_author(pld.msg.guild.me, pld.msg)
+    ender = 'jumps at nothing' if target.id == pld.msg.author.id else f'tackles {target.display_name}'
+    response = discord.Embed(color=0xf9f9f9, title=f'🥋 {auth.display_name} {ender}.')
+    response.set_image(url=interaction['url'])
+    response.set_footer(text=await make_footer(cmd, interaction))
+    await pld.msg.channel.send(embed=response)
+
