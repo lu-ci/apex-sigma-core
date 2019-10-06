edcd4156969923b7f5d0fe1aba728eeea0d0fd7b
Aleksa Radovic <thealeksaradovic@gmail.com>
2019-10-06 15:15:26 +0200

    Vigor Curse Display

--- sigma/modules/utilities/misc/other/event/spooktober/vigor.py
+++ sigma/modules/utilities/misc/other/event/spooktober/vigor.py
@@ -19,6 +19,7 @@ import discord
 
 from sigma.core.utilities.data_processing import user_avatar
 from sigma.modules.utilities.misc.other.event.spooktober.mech.resources.vigor import get_vigor_controller
+from sigma.modules.utilities.misc.other.event.spooktober.mech.util.enchantment import get_curse_controller
 
 
 async def vigor(cmd, pld):
@@ -32,16 +33,26 @@ async def vigor(cmd, pld):
     self = target.id == pld.msg.author.id
     vc = get_vigor_controller(cmd.db)
     vg = await vc.get_vigor(target.id)
-    response = discord.Embed(color=0xbe1931)
-    response.set_author(name=f'❤ {target.display_name}\'s Vigor', icon_url=user_avatar(target))
+    curse_ctrl = get_curse_controller(cmd.db)
+    cursed = await curse_ctrl.is_cursed(target.id)
+    if cursed:
+        response = discord.Embed(color=0x361683)
+        response.set_author(name=f'☠ {target.display_name}\'s Vigor', icon_url=user_avatar(target))
+    else:
+        response = discord.Embed(color=0xbe1931)
+        response.set_author(name=f'❤ {target.display_name}\'s Vigor', icon_url=user_avatar(target))
     starter = 'You have' if self else f'{target.display_name} has'
     referer = 'you' if self else 'them'
+    curse_ref = 'You' if self else 'They'
     tt_chance = await vc.get_chances(target.id, 95)
     tt_cd = await vc.get_cooldown(target.id, 300)
     bar_len = (vg.current * 2) // 10
     empty_len = 20 - bar_len
     bar_text = f'[{"▣" * bar_len}{"▢" * empty_len}] {vg.current}%'
     response.description = f'{starter} **{vg.current}** vigor.'
+    if cursed:
+        curse_timer = await curse_ctrl.get_cursed_time(target.id, True)
+        response.description += f'\n{curse_ref} are cursed for {curse_timer}.'
     response.description += f'\nThat gives {referer} **{tt_chance}%** trick-or-treating luck.'
     response.description += f'\nAnd a **{tt_cd}s** trick-or-treating cooldown.'
     response.description += f'\n```css\n{bar_text}\n```'
