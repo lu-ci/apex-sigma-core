cd61c7887488294fe3808836a766d556dc442abe
AXAz0r <thealeksaradovic@gmail.com>
2019-10-08 14:44:44 +0200

    Invite Link Text Version

--- sigma/modules/help/invite.py
+++ sigma/modules/help/invite.py
@@ -26,8 +26,12 @@ async def invite(cmd, pld):
     :param pld: The payload with execution data and details.
     :type pld: sigma.core.mechanics.payload.CommandPayload
     """
-    inv_title = 'Click here to invite me.'
-    sigma_image = 'https://i.imgur.com/mGyqMe1.png'
     invite_url = f'https://discordapp.com/oauth2/authorize?client_id={cmd.bot.user.id}&scope=bot&permissions=8'
-    response = discord.Embed(color=0x1B6F5F).set_author(name=inv_title, icon_url=sigma_image, url=invite_url)
-    await pld.msg.channel.send(embed=response)
+    if 'text' in ' '.join(pld.args).lower():
+        invite_text = f'Click the following link to invite me: <{invite_url}>'
+        await pld.msg.channel.send(invite_text)
+    else:
+        inv_title = 'Click here to invite me.'
+        sigma_image = 'https://i.imgur.com/mGyqMe1.png'
+        response = discord.Embed(color=0x1B6F5F).set_author(name=inv_title, icon_url=sigma_image, url=invite_url)
+        await pld.msg.channel.send(embed=response)
