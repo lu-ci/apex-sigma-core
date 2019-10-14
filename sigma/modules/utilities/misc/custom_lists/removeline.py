56bcaf59b7eda8ca7cbd94521d7265f978574f14
Daniel Preston <prestondj.2001@yahoo.com>
2019-10-14 18:00:32 -0400

    Allow line removal for public lists

--- sigma/modules/utilities/misc/custom_lists/removeline.py
+++ sigma/modules/utilities/misc/custom_lists/removeline.py
@@ -18,7 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import discord
 
-from sigma.core.utilities.generic_responses import denied, error, not_found
+from sigma.core.utilities.generic_responses import error, not_found
+from sigma.modules.utilities.misc.custom_lists.addline import user_auth
 
 
 async def removeline(cmd, pld):
@@ -28,30 +29,28 @@ async def removeline(cmd, pld):
     :param pld: The payload with execution data and details.
     :type pld: sigma.core.mechanics.payload.CommandPayload
     """
-    if pld.args:
-        if len(pld.args) > 1:
-            list_coll = cmd.db[cmd.db.db_nam].CustomLists
-            lookup_data = {'server_id': pld.msg.guild.id, 'list_id': pld.args[0].lower()}
-            list_file = await list_coll.find_one(lookup_data)
-            if list_file:
-                author_id = list_file.get('user_id')
-                if author_id == pld.msg.author.id:
-                    line = pld.args[1]
-                    if line.isdigit():
-                        try:
-                            list_file.get('contents', []).pop(int(line) - 1)
-                            await list_coll.update_one(lookup_data, {'$set': list_file})
-                            response = discord.Embed(color=0xFFCC4D, title=f'🔥 Line {line} was deleted.')
-                        except IndexError:
-                            response = not_found('Line not found.')
-                    else:
-                        response = error('Invalid line number.')
+    if len(pld.args) > 1:
+        list_coll = cmd.db[cmd.db.db_nam].CustomLists
+        lookup_data = {'server_id': pld.msg.guild.id, 'list_id': pld.args[0].lower()}
+        list_file = await list_coll.find_one(lookup_data)
+        if list_file:
+            if user_auth(pld.msg, list_file):
+                line = pld.args[1]
+                if line.isdigit():
+                    try:
+                        line_num = max(1, int(line))
+                        list_file.get('contents', []).pop(line_num - 1)
+                        await list_coll.update_one(lookup_data, {'$set': list_file})
+                        response = discord.Embed(color=0xFFCC4D, title=f'🔥 Line {line_num} was deleted.')
+                    except IndexError:
+                        response = not_found('Line not found.')
                 else:
-                    response = denied('You didn\'t make this list.')
+                    response = error('Invalid line number.')
             else:
-                response = error('Missing or invalid list ID.')
+                mode = 'private' if list_file.get('mode') == 'private' else 'locked'
+                response = discord.Embed(color=0xFFAC33, title=f'🔏 This list is {mode}.')
         else:
-            response = error('Not enough arguments.')
+            response = error('Missing or invalid list ID.')
     else:
-        response = error('Nothing inputted.')
+        response = error('Not enough arguments.')
     await pld.msg.channel.send(embed=response)
