917bbe87da4772311d9f34c85aa07e09c51d9cbf
AXAz0r <thealeksaradovic@gmail.com>
2019-10-17 14:35:20 +0200

    Guild and User Join Dumpers

---
+++ sigma/modules/core_functions/storage/user_joiner.py
@@ -0,0 +1,31 @@
+"""
+Apex Sigma: The Database Giant Discord Bot.
+Copyright (C) 2019  Lucia's Cipher
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from sigma.core.mechanics.fetch import get_fetch_helper
+
+
+async def user_joiner(ev, pld):
+    """
+    :param ev: The event object referenced in the event.
+    :type ev: sigma.core.mechanics.event.SigmaEvent
+    :param pld: The event payload data to process.
+    :type pld: sigma.core.mechanics.payload.MemberPayload
+    """
+    variant = 'user'
+    fh = get_fetch_helper(ev.bot)
+    data = fh.make_user_data(pld.member)
+    await fh.save_object_doc(variant, data)
