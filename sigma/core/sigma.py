64b99b17c36d2892b1cd5934f442425a1d6e4b6c
Shifty <prestondj.2001@yahoo.com>
2020-10-27 11:56:45 -0400

    Revert "Pass intents to client"
    This reverts commit 8e792891831eb78553c2441c69da1d28fcc7d3fe.

--- sigma/core/sigma.py
+++ sigma/core/sigma.py
@@ -68,8 +68,7 @@ class ApexSigma(client_class):
     )
 
     def __init__(self):
-        intents = discord.Intents.all()
-        super().__init__(status=discord.Status.dnd, activity=discord.Game('booting...'), intents=intents)
+        super().__init__(status=discord.Status.dnd, activity=discord.Game('booting...'))
         self.ready = False
         # State attributes before initialization.
         self.log = self.init_logger()
