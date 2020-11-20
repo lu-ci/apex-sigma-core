9bee8cdd4eaae6923d1be6f642d421554bfcddda
AXAz0r <thealeksaradovic@gmail.com>
2020-11-20 13:23:21 +0100

    Reduce Upgrade Prices

--- sigma/modules/minigames/professions/nodes/upgrade_params.py
+++ sigma/modules/minigames/professions/nodes/upgrade_params.py
@@ -20,37 +20,37 @@ upgrade_list = [
     {
         'id': 'stamina',
         'name': 'Endurance Training',
-        'cost': 300,
+        'cost': 75,
         'desc': 'Reduces profession cool-downs.'
     },
     {
         'id': 'luck',
         'name': 'Lucky Enchantment',
-        'cost': 400,
+        'cost': 100,
         'desc': 'Increases the chance for rare items'
     },
     {
         'id': 'storage',
         'name': 'Inventory Expansion',
-        'cost': 100,
+        'cost': 25,
         'desc': 'Increases your inventory space by 8.'
     },
     {
         'id': 'oven',
         'name': 'Super Heated Oven',
-        'cost': 150,
+        'cost': 35,
         'desc': 'Decreases the cookie giving cool-down.'
     },
     {
         'id': 'casino',
         'name': 'Casino Investment',
-        'cost': 300,
+        'cost': 75,
         'desc': 'Reduces the slot machine cool-down.'
     },
     {
         'id': 'harem',
         'name': 'Harem Expansion',
-        'cost': 500,
+        'cost': 125,
         'desc': 'Increases your harem size by 1.'
     }
 ]
