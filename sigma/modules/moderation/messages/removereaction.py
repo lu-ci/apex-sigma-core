4dd4142438e1a4421ad7fad2bfe45447d91f36e6
Shifty <prestondj.2001@yahoo.com>
2021-02-02 23:46:48 -0500

    Remove yet another unused import

--- sigma/modules/moderation/messages/removereaction.py
+++ sigma/modules/moderation/messages/removereaction.py
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import discord
-
 from sigma.core.utilities.generic_responses import denied, error, not_found, ok
 from sigma.modules.utilities.misc.other.quote import message_search
 
