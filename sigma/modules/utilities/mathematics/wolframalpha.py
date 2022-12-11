55b009d3ed8541a1809e8d763ba4a8b354c01586
Shifty <prestondj.2001@yahoo.com>
2022-12-10 21:54:28 -0500

    Wolfram fix

--- sigma/modules/utilities/mathematics/wolframalpha.py
+++ sigma/modules/utilities/mathematics/wolframalpha.py
@@ -31,7 +31,7 @@ api_url = 'http://api.wolframalpha.com/v2/query?output=JSON&format=image,plainte
 class WolframResult(object):
     def __init__(self, data):
         self.pods = [Pod(p) for p in data.get('pods', [])]
-        self.primary_pod = list(filter(lambda x: x.is_primary, self.pods))[0]
+        self.primary_pod = list(filter(lambda x: x.is_primary, self.pods))[0] if self.pods else []
         self.success = data.get('success') and bool(self.primary_pod)
 
 
