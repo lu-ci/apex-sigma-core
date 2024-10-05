d84306808b7b59ba47f9c43a9a509f6c0f1b7c0e
Shifty <prestondj.2001@yahoo.com>
2024-10-05 01:05:17 -0400

    Actually add the command file...

---
+++ sigma/modules/moderation/server_settings/collectionjar/clearcollectionjar.py
@@ -0,0 +1,40 @@
+"""
+Apex Sigma: The Database Giant Discord Bot.
+Copyright (C) 2019  Lucia's Cipher
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import discord
+
+from sigma.core.utilities.generic_responses import GenericResponse
+
+
+async def clearcollectionjar(cmd, pld):
+    """
+    :param cmd: The command object referenced in the command.
+    :type cmd: sigma.core.mechanics.command.SigmaCommand
+    :param pld: The payload with execution data and details.
+    :type pld: sigma.core.mechanics.payload.CommandPayload
+    """
+    if pld.msg.channel.permissions_for(pld.msg.author).manage_guild:
+        jar_doc = pld.settings.get('collection_jar')
+        if jar_doc:
+            await cmd.db.set_guild_settings(pld.msg.guild.id, 'collection_jar', {})
+            response = GenericResponse(f'Collection Jar cleared and trigger reset.').ok()
+        else:
+                response = GenericResponse('No Collection Jar data found on this server.').not_found()
+    else:
+        response = GenericResponse('Access Denied. Manage Server needed.').denied()
+    await pld.msg.channel.send(embed=response)
