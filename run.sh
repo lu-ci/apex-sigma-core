0092a280d219bd247c33084eb5dc13958c96b5a8
Aleksa Radovic <thealeksaradovic@gmail.com>
2026-08-25 14:39:51 +0200

    Remove Restart Arg

--- run.sh
+++ run.sh
@@ -32,4 +32,4 @@ total_count=$SIGMA_SHARD_COUNT
 shard_ids=$SIGMA_SHARD_GROUP
 wait_time=$((SIGMA_SHARD_GROUP * 5))
 sleep $wait_time
-exec uv run ./run.py --count "$total_count" --group "$shard_ids" --restart
+exec uv run ./run.py --count "$total_count" --group "$shard_ids"
