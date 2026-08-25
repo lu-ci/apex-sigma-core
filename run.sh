455c35e6a31f487c9734cc3c7731be76bc956f58
Aleksa Radovic <thealeksaradovic@gmail.com>
2026-08-25 12:13:47 +0200

    Old Grouped Sharding

--- run.sh
+++ run.sh
@@ -6,24 +6,28 @@ set -e
 unset CDPATH
 cd "$(dirname "$0")"
 
-if [ -z "$SIGMA_SHARD_COUNT" ] || [ -z "$SIGMA_SHARD_GROUP" ]; then
-  echo "Environment variables not set. Running script with no arguments..."
-  exec uv run ./run.py
-  exit 0
-fi
+#if [ -z "$SIGMA_SHARD_COUNT" ] || [ -z "$SIGMA_SHARD_GROUP" ]; then
+#  echo "Environment variables not set. Running script with no arguments..."
+#  exec uv run ./run.py
+#  exit 0
+#fi
+#
+#total_count=$SIGMA_SHARD_COUNT
+#group_size=$SIGMA_SHARD_GROUP
+#
+#num_groups=$((total_count / group_size))
+#
+#for ((i = 0; i < num_groups; i++)); do
+#  start=$((i * group_size))
+#  end=$((start + group_size - 1))
+#  shard_ids=$(seq -s, $start $end)
+#
+#  exec uv run ./run.py --count "$total_count" --group "$shard_ids" --restart &
+#  sleep 5
+#done
+#
+#wait
 
 total_count=$SIGMA_SHARD_COUNT
-group_size=$SIGMA_SHARD_GROUP
-
-num_groups=$((total_count / group_size))
-
-for ((i = 0; i < num_groups; i++)); do
-  start=$((i * group_size))
-  end=$((start + group_size - 1))
-  shard_ids=$(seq -s, $start $end)
-
-  exec uv run ./run.py --count "$total_count" --group "$shard_ids" --restart &
-  sleep 5
-done
-
-wait
+shard_ids=$SIGMA_SHARD_GROUP
+exec uv run ./run.py --count "$total_count" --group "$shard_ids" --restart
